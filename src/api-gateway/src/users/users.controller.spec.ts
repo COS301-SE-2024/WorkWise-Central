15b6e1ecb91ce5a2282dbcc160f3f1d617683182
Hamza Mokiwa <u22575172@tuks.co.za>
2024-05-23 22:47:30 +0200

    Imports mongoose library into usersModule

--- src/api-gateway/src/users/users.controller.spec.ts
+++ src/api-gateway/src/users/users.controller.spec.ts
@@ -1,20 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { userStub } from '../../test/stubs/user.stub';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('UsersController', () => {
-  let controller: UsersController;
-
+  let usersController: UsersController;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+    })
+        .useMocker((token) => {
+          if (token === UsersService) {
+            return {
+              create: jest.fn().mockReturnValue(userStub()),
+              findAllUsers: jest.fn().mockReturnValue(userStub()),
+              findUser: jest.fn().mockReturnValue(userStub()),
+              update: jest.fn().mockReturnValue(userStub()),
+              remove: jest.fn().mockReturnValue(userStub()),
+            };
+          }
+          if (typeof token === 'function') {
+            const mockMetadata = moduleMocker.getMetadata(
+                token,
+            ) as MockFunctionMetadata<any, any>;
+            const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+            return new Mock();
+          }
+        })
+        .compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get(UsersController);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  it('should create a user', function () {
+    expect(usersController.create(userStub())).toBeDefined();
+    expect(usersController.create(userStub())).toStrictEqual(userStub());
+  });
+
+  it('should find all users', function () {
+    expect(usersController.findAll()).toBeDefined();
   });
 });
--- src/api-gateway/src/users/users.module.ts
+++ src/api-gateway/src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import {MongooseModule} from "@nestjs/mongoose";
+import {UserSchema} from "./entities/user.entity";
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  controllers: [UsersController],
+  imports:[
+    ConfigModule.forRoot({envFilePath:`.env`}),
+    MongooseModule.forRoot(`${process.env.SERVER_LOGIN}`),
+    MongooseModule.forFeature([{name:'User', schema:UserSchema}])],
   providers: [UsersService],
 })
 export class UsersModule {}
