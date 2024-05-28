d1c76c819374cfa25ad4d5f3630cc01e3088a325
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-05-29 01:19:45 +0200

    Created modals for sign up and log in
    I have created the sign up and log in modals. When you press the buttons, sign up and log in their modals will appear. the splash page is has most of its components, there's styling that needs to be fixed as i need to change the color of the buttons to the color palette

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import SignupView from '../views/SignupView.vue'
 import SplashView from '../views/SplashView.vue'
 
 const router = createRouter({
@@ -8,18 +6,8 @@ const router = createRouter({
   routes: [
     {
       path: '/home',
-      name:'home',
+      name: 'home',
       component: SplashView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path:'/signup',
-      name:'signup',
-      component: SignupView
     }
   ]
 })
