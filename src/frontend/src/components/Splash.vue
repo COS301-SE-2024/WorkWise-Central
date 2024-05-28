3f6dfe1bfedcf8fc51616eae0d6e4ed2ce4b2df7
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-05-29 01:19:45 +0200

    Created modals for sign up and log in
    I have created the sign up and log in modals. When you press the buttons, sign up and log in their modals will appear. the splash page is has most of its components, there's styling that needs to be fixed as i need to change the color of the buttons to the color palette

--- src/frontend/src/components/Splash.vue
+++ src/frontend/src/components/Splash.vue
@@ -1,73 +1,182 @@
 <template>
-  <div>
-    <h1><span class="header-title">Work</span><span class="color2">Wise</span></h1>
-  </div>
-  <div class=".bodyAlignment">
-    <h1>
-      Welcome to <span class="header-title">Work</span
-      ><span class="header-title2">Wise</span> Central
-    </h1>
-    <VBtn class="elevation-2" style="color">Log in</VBtn>
-    <span></span>
-    <VBtn class="elevation-2" style="color2">Sign up</VBtn>
-    <span></span>
-  </div>
-  <div class="imageAlignment">
-    <img
-      src="C:\Users\Kumbirai\OneDrive\Documents\GitHub\WorkWise-Central\src\frontend\img\splash.jpg"
-      alt="work chat"
-    />
-  </div>
+  <v-container class="fill-height text center d-flex-column justify-center align-center">
+    <v-row>
+      <v-col cols="6">
+        <div>
+          <h1 class="splash-title header-title">
+            Welcome To <span class="colorAccent">Work</span
+            ><span class="colorAccent2">Wise</span> Central
+          </h1>
+        </div>
+
+        <div>
+          <v-btn
+            color="colorAccent"
+            dark
+            @click="loginDialog = true"
+            rounded="xl"
+            align-center
+            class="my-3 button-width button-height"
+            >Log in</v-btn
+          >
+          <v-dialog v-model="loginDialog" max-width="500">
+            <v-card prepend-icon="mdi-account" title="Log into existing account" rounded="xl">
+              <v-card-text>
+                <v-text-field
+                  label="Email"
+                  prepend-icon="mdi-email"
+                  type="email"
+                  v-model="email"
+                ></v-text-field>
+                <v-text-field
+                  label="Password"
+                  prepend-icon="mdi-lock"
+                  type="password"
+                  v-model="password"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn text @click="login">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+        <v-btn
+          color="primary"
+          dark
+          @click="signupDialog = true"
+          rounded="xl"
+          align-center
+          class="my-3 button-width button-height"
+          >Sign up</v-btn
+        >
+        <v-dialog v-model="signupDialog" max-width="500" max-height="400">
+          <v-card
+            prepend-icon="mdi-account"
+            title="Create an account"
+            rounded="xl"
+            max-height="600"
+          >
+            <v-card-text>
+              <v-text-field
+                label="Enter your email"
+                prepend-icon="mdi-email"
+                type="email"
+                v-model="email"
+                aria-placeholder="Enter your email address"
+              ></v-text-field>
+              <v-text-field
+                label="Enter your password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="password"
+                aria-placeholder="Enter your password"
+              ></v-text-field>
+              <v-text-field
+                label="Confirm your Password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="password"
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click="nextFlow" rounded="xl">Sign up</v-btn>
+              <v-btn color="primary" @click="login" rounded="xl">Log in</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+    <v-col cols="6">
+      <img
+        class="splash-image"
+        src="C:\Users\Kumbirai\OneDrive\Documents\GitHub\WorkWise-Central\src\frontend\img\splash.jpg"
+      />
+    </v-col>
+  </v-container>
 </template>
+<script>
+import {
+  VAppBar,
+  VBtn,
+  VTextField,
+  VCard,
+  VDialog,
+  VCardActions,
+  VCardText,
+  VSpacer,
+  VCardTitle,
+  VContainer,
+  VCol,
+  VRow,
+  VApp
+} from 'vuetify/components'
 
-<style scoped>
-.bodyAlignment {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  height: 100vh;
-}
-
-.imageAlignment {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  justify-content: center;
-  height: 100vh;
-}
-.input {
-  font-size: 12px;
-  font-family: Lato;
-  font-weight: normal;
-}
-.header-title {
-  font-size: 36px;
-  font-family: 'Lato';
-  font-weight: normal;
-  color: #6a99ce;
-}
-.header-title2 {
-  font-size: 36px;
-  font-family: 'Lato';
-  font-weight: normal;
-  color: 879898;
-}
-
-.color2 {
-  color: #879898;
-}
-.color {
-  color: #6a99ce;
-}
-</style>
-<script lang="ts">
-import { VAppBar, VBtn, VSpacer } from 'vuetify/lib/components/index.mjs'
 export default {
+  data: () => ({
+    loginDialog: false,
+    signupDialog: false,
+    email: '',
+    password: ''
+  }),
+  methods: {
+    login() {
+      // Implement login logic here
+    },
+    nextFlow() {},
+    openDialog() {
+      this.dialog = true
+    }
+  },
   components: {
     VAppBar,
     VBtn,
-    VSpacer
+    VTextField,
+    VCard,
+    VDialog,
+    VCardActions,
+    VCardText,
+    VSpacer,
+    VCardTitle,
+    VContainer,
+    VCol,
+    VRow,
+    VApp
   }
 }
 </script>
+<style scoped>
+.header-title {
+  font-size: 40px;
+  font: 'Lato';
+}
+.fill-height {
+  height: 100%;
+}
+
+.button-width {
+  width: 400px;
+}
+
+.button-height {
+  height: 100px;
+}
+
+.colorAccent {
+  color: #6a99ce;
+}
+.colorAccent2 {
+  color: #879898;
+}
+.text-start {
+  text-align: start;
+}
+
+.splash-image {
+  width: 140%;
+  height: 100%;
+  align-content: end;
+}
+</style>
