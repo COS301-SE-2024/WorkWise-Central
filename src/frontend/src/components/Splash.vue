7712549f56f41fc3f84619d0b39fe4c47b478a72
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-05-28 16:05:26 +0200

    Splash page designs
    implemented the basic log in and sign up as well as the splash page. They still need to be worked on however the basic format has been implemented

---
+++ src/frontend/src/components/Splash.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h1><span class="header-title">Work</span><span class="color2">Wise</span></h1>
+  </div>
+  <div class=".bodyAlignment">
+    <h1>
+      Welcome to <span class="header-title">Work</span
+      ><span class="header-title2">Wise</span> Central
+    </h1>
+    <VBtn class="elevation-2" style="color">Log in</VBtn>
+    <span></span>
+    <VBtn class="elevation-2" style="color2">Sign up</VBtn>
+    <span></span>
+  </div>
+  <div class="imageAlignment">
+    <img
+      src="C:\Users\Kumbirai\OneDrive\Documents\GitHub\WorkWise-Central\src\frontend\img\splash.jpg"
+      alt="work chat"
+    />
+  </div>
+</template>
+
+<style scoped>
+.bodyAlignment {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  height: 100vh;
+}
+
+.imageAlignment {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  height: 100vh;
+}
+.input {
+  font-size: 12px;
+  font-family: Lato;
+  font-weight: normal;
+}
+.header-title {
+  font-size: 36px;
+  font-family: 'Lato';
+  font-weight: normal;
+  color: #6a99ce;
+}
+.header-title2 {
+  font-size: 36px;
+  font-family: 'Lato';
+  font-weight: normal;
+  color: 879898;
+}
+
+.color2 {
+  color: #879898;
+}
+.color {
+  color: #6a99ce;
+}
+</style>
+<script lang="ts">
+import { VAppBar, VBtn, VSpacer } from 'vuetify/lib/components/index.mjs'
+export default {
+  components: {
+    VAppBar,
+    VBtn,
+    VSpacer
+  }
+}
+</script>
--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,11 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const vuetify = createVuetify({})
+createApp(App)
+    .use(router)
+    .use(vuetify)
+    .mount('#app')
