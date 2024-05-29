371c398d6604611b7ffe3c6cd66c1b783334e138
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-05-29 15:39:58 +0200

    Added the rest of the sign up flow modals
    Create the rest of the sign up flow modals. You can toggle between the sign up flows and between sign and log in

--- src/frontend/src/components/Splash.vue
+++ src/frontend/src/components/Splash.vue
@@ -38,6 +38,7 @@
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn text @click="login">Login</v-btn>
+                <v-btn text @click="(signupDialog = true)((loginDialog = false))">Sign up</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
@@ -82,8 +83,100 @@
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn text @click="nextFlow" rounded="xl">Sign up</v-btn>
-              <v-btn color="primary" @click="login" rounded="xl">Log in</v-btn>
+              <v-btn
+                text
+                @click="(signupDialog = false), (signup1Dialog = true)"
+                rounded="xl"
+                boarder="xl"
+                >Sign up</v-btn
+              >
+              <v-btn
+                color="primary"
+                @click="(loginDialog = true), (signupDialog = false)"
+                rounded="xl"
+                >Log in</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="signup1Dialog" max-width="500" max-height="400">
+          <v-card
+            prepend-icon="mdi-account"
+            title="Create your profile"
+            rounded="xl"
+            max-height="600"
+          >
+            <v-card-text>
+              <v-text-field
+                label="Enter your name"
+                prepend-icon="mdi-name"
+                type="input"
+                v-model="name"
+              ></v-text-field>
+              <v-text-field
+                label="Enter your surname"
+                prepend-icon="mdi-surname"
+                type="input"
+                v-model="surname"
+                aria-placeholder="Enter your password"
+              ></v-text-field>
+              <v-text-field
+                label="Enter your display name"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="display_name"
+              ></v-text-field>
+              <v-avatar label="Upload your profile pricture" type="file" v-model="image">
+                <!-- <v-img src="C:\Users\Kumbirai\OneDrive\Documents\GitHub\WorkWise-Central\src\frontend\img\avatar.jpg"></v-img> -->
+              </v-avatar>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click="(signup1Dialog = false)((signup2Dialog = true))" rounded="xl"
+                >Continue</v-btn
+              >
+              <v-btn
+                color="primary"
+                @click="(signupDialog = true)((signup1Dialog = false))"
+                rounded="xl"
+                >Back</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="signup2Dialog" max-width="500" max-height="400">
+          <v-card
+            prepend-icon="mdi-account"
+            title="Create your profile"
+            rounded="xl"
+            max-height="600"
+          >
+            <v-card-text>
+              <v-text-field
+                label="Date of birth"
+                prepend-icon="mdi-birth"
+                type="input"
+                v-model="date"
+              ></v-text-field>
+              <v-select label="Gender" items="['Male','Female','Other']" hint="Chose your gender">
+              </v-select>
+              <v-select
+                label="Preferred Lanaguage"
+                items="['English','French','Shona']"
+                hint="Chose your preferred lanaguage"
+              >
+              </v-select>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click="(signup2Dialog = false)(signup)" rounded="xl">Continue</v-btn>
+              <v-btn
+                color="primary"
+                @click="(signup1Dialog = true)((signup2Dialog = false))"
+                rounded="xl"
+                >Back</v-btn
+              >
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -111,20 +204,25 @@ import {
   VContainer,
   VCol,
   VRow,
-  VApp
+  VApp,
+  VAvatar,
+  VImg,
+  VSelect
 } from 'vuetify/components'
 
 export default {
   data: () => ({
     loginDialog: false,
     signupDialog: false,
+    signup1Dialog: false,
+    signup2Dialog: false,
+
     email: '',
     password: ''
   }),
   methods: {
-    login() {
-      // Implement login logic here
-    },
+    login() {},
+    signup() {},
     nextFlow() {},
     openDialog() {
       this.dialog = true
@@ -143,7 +241,10 @@ export default {
     VContainer,
     VCol,
     VRow,
-    VApp
+    VApp,
+    VAvatar,
+    VImg,
+    VSelect
   }
 }
 </script>
