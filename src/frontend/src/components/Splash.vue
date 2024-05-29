cd1621a89e3c1bbadfbe3528f8cb242671f138a3
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-05-29 23:07:14 +0200

    Styling update
    I have updated the styling of the splash page and the sign up components. I need to add the color changes and the dark mode

--- src/frontend/src/components/Splash.vue
+++ src/frontend/src/components/Splash.vue
@@ -1,194 +1,234 @@
 <template>
-  <v-container class="fill-height text center d-flex-column justify-center align-center">
-    <v-row>
-      <v-col cols="6">
-        <div>
-          <h1 class="splash-title header-title">
-            Welcome To <span class="colorAccent">Work</span
-            ><span class="colorAccent2">Wise</span> Central
-          </h1>
-        </div>
+  <v-layout>
+    <!-- Toolbar -->
+    <v-app-bar app color="white" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        <span class="colorAccent toolbar-text">Work</span>
+        <span class="colorAccent2 toolbar-text">Wise</span>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-switch label="Theme"></v-switch>
+    </v-app-bar>
+    <!-- Main Content -->
+    <v-main>
+      <v-container fluid fill-height>
+        <v-row>
+          <!-- Left Half -->
+          <v-col cols="6" align-self="center">
+            <div>
+              <h1 class="splash-title header-title">
+                Welcome To <span class="colorAccent">Work</span>
+                <span class="colorAccent2">Wise</span> Central
+              </h1>
+            </div>
+            <div>
+              <v-btn
+                color="colorAccent"
+                dark
+                @click="loginDialog = true"
+                rounded="xl"
+                align-center
+                class="my-3 button-width button-height"
+              >
+                Log in
+              </v-btn>
+              <v-dialog v-model="loginDialog" max-width="500" style="height: 700px">
+                <v-sheet></v-sheet>
+                <v-card title="Log into existing account" rounded="xl">
+                  <v-card-text>
+                    <v-row dense>
+                      <v-col>
+                        <v-text-field
+                          label="Email"
+                          type="email"
+                          v-model="email"
+                          rounded="xl"
+                          variant="solo"
+                        ></v-text-field>
+                        <v-text-field
+                          label="Password"
+                          type="password"
+                          v-model="password"
+                          rounded="xl"
+                          variant="solo"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                    <v-btn text @click="login">Login</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn text @click="(signupDialog = true)((loginDialog = false))"
+                      >Sign up</v-btn
+                    >
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-btn
+                color="primary"
+                dark
+                @click="signupDialog = true"
+                rounded="xl"
+                align-center
+                class="my-3 button-width button-height"
+              >
+                Sign up
+              </v-btn>
+              <v-dialog v-model="signupDialog" max-width="500" max-height="400">
+                <v-sheet elevation="14" rounded="xl">
+                  <v-card title="Create an account" rounded="xl" max-height="600">
+                    <v-card-text>
+                      <v-text-field
+                        label="Enter your email"
+                        type="email"
+                        v-model="email"
+                        rounded="xl"
+                        variant="solo"
+                        aria-placeholder="Enter your email address"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Enter your password"
+                        type="password"
+                        v-model="password"
+                        rounded="xl"
+                        variant="solo"
+                        aria-placeholder="Enter your password"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Confirm your Password"
+                        type="password"
+                        v-model="password"
+                        rounded="xl"
+                        variant="solo"
+                      ></v-text-field>
+                    </v-card-text>
+                    <v-col class="d-flex justify-center align-center">
+                      <v-btn
+                        text
+                        @click="(signupDialog = false), (signup1Dialog = true)"
+                        rounded="xl"
+                        boarder="xl"
+                        >Sign up</v-btn
+                      >
 
-        <div>
-          <v-btn
-            color="colorAccent"
-            dark
-            @click="loginDialog = true"
-            rounded="xl"
-            align-center
-            class="my-3 button-width button-height"
-            >Log in</v-btn
-          >
-          <v-dialog v-model="loginDialog" max-width="500">
-            <v-card prepend-icon="mdi-account" title="Log into existing account" rounded="xl">
-              <v-card-text>
-                <v-text-field
-                  label="Email"
-                  prepend-icon="mdi-email"
-                  type="email"
-                  v-model="email"
-                ></v-text-field>
-                <v-text-field
-                  label="Password"
-                  prepend-icon="mdi-lock"
-                  type="password"
-                  v-model="password"
-                ></v-text-field>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn text @click="login">Login</v-btn>
-                <v-btn text @click="(signupDialog = true)((loginDialog = false))">Sign up</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </div>
-        <v-btn
-          color="primary"
-          dark
-          @click="signupDialog = true"
-          rounded="xl"
-          align-center
-          class="my-3 button-width button-height"
-          >Sign up</v-btn
-        >
-        <v-dialog v-model="signupDialog" max-width="500" max-height="400">
-          <v-card
-            prepend-icon="mdi-account"
-            title="Create an account"
-            rounded="xl"
-            max-height="600"
-          >
-            <v-card-text>
-              <v-text-field
-                label="Enter your email"
-                prepend-icon="mdi-email"
-                type="email"
-                v-model="email"
-                aria-placeholder="Enter your email address"
-              ></v-text-field>
-              <v-text-field
-                label="Enter your password"
-                prepend-icon="mdi-lock"
-                type="password"
-                v-model="password"
-                aria-placeholder="Enter your password"
-              ></v-text-field>
-              <v-text-field
-                label="Confirm your Password"
-                prepend-icon="mdi-lock"
-                type="password"
-                v-model="password"
-              ></v-text-field>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                text
-                @click="(signupDialog = false), (signup1Dialog = true)"
-                rounded="xl"
-                boarder="xl"
-                >Sign up</v-btn
-              >
-              <v-btn
-                color="primary"
-                @click="(loginDialog = true), (signupDialog = false)"
-                rounded="xl"
-                >Log in</v-btn
-              >
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="signup1Dialog" max-width="500" max-height="400">
-          <v-card
-            prepend-icon="mdi-account"
-            title="Create your profile"
-            rounded="xl"
-            max-height="600"
-          >
-            <v-card-text>
-              <v-text-field
-                label="Enter your name"
-                prepend-icon="mdi-name"
-                type="input"
-                v-model="name"
-              ></v-text-field>
-              <v-text-field
-                label="Enter your surname"
-                prepend-icon="mdi-surname"
-                type="input"
-                v-model="surname"
-                aria-placeholder="Enter your password"
-              ></v-text-field>
-              <v-text-field
-                label="Enter your display name"
-                prepend-icon="mdi-lock"
-                type="password"
-                v-model="display_name"
-              ></v-text-field>
-              <v-avatar label="Upload your profile pricture" type="file" v-model="image">
-                <!-- <v-img src="C:\Users\Kumbirai\OneDrive\Documents\GitHub\WorkWise-Central\src\frontend\img\avatar.jpg"></v-img> -->
-              </v-avatar>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn text @click="(signup1Dialog = false)((signup2Dialog = true))" rounded="xl"
-                >Continue</v-btn
-              >
-              <v-btn
-                color="primary"
-                @click="(signupDialog = true)((signup1Dialog = false))"
-                rounded="xl"
-                >Back</v-btn
-              >
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+                      <v-btn
+                        color="primary"
+                        @click="(loginDialog = true), (signupDialog = false)"
+                        rounded="xl"
+                        >Log in</v-btn
+                      >
+                    </v-col>
+                  </v-card>
+                </v-sheet>
+              </v-dialog>
+              <v-dialog v-model="signup1Dialog" max-width="500" max-height="400">
+                <v-sheet elevation="14" rounded="xl">
+                  <v-card title="Create your profile" rounded="xl" max-height="600">
+                    <v-card-text>
+                      <v-text-field
+                        label="Enter your name"
+                        type="input"
+                        v-model="name"
+                        rounded="xl"
+                        variant="solo"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Enter your surname"
+                        type="input"
+                        v-model="surname"
+                        rounded="xl"
+                        variant="solo"
+                        aria-placeholder="Enter your password"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Enter your display name"
+                        type="input"
+                        v-model="display_name"
+                        rounded="xl"
+                        variant="solo"
+                      ></v-text-field>
+                    </v-card-text>
+                    <v-col class="d-flex justify-center align-center">
+                      <v-btn
+                        text
+                        @click="(signup1Dialog = false)((signup2Dialog = true))"
+                        rounded="xl"
+                        >Continue</v-btn
+                      >
+                      <v-btn
+                        color="primary"
+                        @click="(signupDialog = true)((signup1Dialog = false))"
+                        rounded="xl"
+                        >Back</v-btn
+                      >
+                    </v-col>
+                  </v-card>
+                </v-sheet>
+              </v-dialog>
 
-        <v-dialog v-model="signup2Dialog" max-width="500" max-height="400">
-          <v-card
-            prepend-icon="mdi-account"
-            title="Create your profile"
-            rounded="xl"
-            max-height="600"
-          >
-            <v-card-text>
-              <v-text-field
-                label="Date of birth"
-                prepend-icon="mdi-birth"
-                type="input"
-                v-model="date"
-              ></v-text-field>
-              <v-select label="Gender" items="['Male','Female','Other']" hint="Chose your gender">
-              </v-select>
-              <v-select
-                label="Preferred Lanaguage"
-                items="['English','French','Shona']"
-                hint="Chose your preferred lanaguage"
-              >
-              </v-select>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn text @click="(signup2Dialog = false)(signup)" rounded="xl">Continue</v-btn>
-              <v-btn
-                color="primary"
-                @click="(signup1Dialog = true)((signup2Dialog = false))"
+              <v-dialog v-model="signup2Dialog" max-width="500" max-height="400">
+                <v-sheet elevation="14" rounded="xl">
+                  <v-card title="Create your profile" rounded="xl" max-height="600">
+                    <v-card-text>
+                      <v-text-field
+                        label="Date of birth"
+                        type="input"
+                        v-model="date"
+                        rounded="xl"
+                        variant="solo"
+                        clearable
+                      ></v-text-field>
+                      <v-select
+                        label="Gender"
+                        hint="Chose your gender"
+                        rounded="xl"
+                        variant="solo"
+                        clearable
+                      >
+                      </v-select>
+                      <v-select
+                        label="Preferred Language"
+                        hint="Chose your preferred language"
+                        rounded="xl"
+                        variant="solo"
+                        clearable
+                      >
+                      </v-select>
+                    </v-card-text>
+                    <v-col class="d-flex justify-center align-center">
+                      <v-btn text @click="(signup2Dialog = false)(signup)" rounded="xl"
+                        >Continue</v-btn
+                      >
+                      <v-btn
+                        color="primary"
+                        @click="(signup1Dialog = true)((signup2Dialog = false))"
+                        rounded="xl"
+                        >Back</v-btn
+                      >
+                    </v-col>
+                  </v-card>
+                </v-sheet>
+              </v-dialog>
+            </div>
+          </v-col>
+
+          <!-- Right Half -->
+          <v-col cols="6">
+            <div class="w-full h-full">
+              <v-img
+                src="/img/splash.jpg"
+                alt="splash"
+                class="w-full h-full object-cover"
                 rounded="xl"
-                >Back</v-btn
-              >
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-col>
-    </v-row>
-    <v-col cols="6">
-      <img
-        class="splash-image"
-        src="C:\Users\Kumbirai\OneDrive\Documents\GitHub\WorkWise-Central\src\frontend\img\splash.jpg"
-      />
-    </v-col>
-  </v-container>
+              ></v-img>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-layout>
 </template>
 <script>
 import {
@@ -207,7 +247,15 @@ import {
   VApp,
   VAvatar,
   VImg,
-  VSelect
+  VSelect,
+  VToolbar,
+  VToolbarTitle,
+  VAppBarNavIcon,
+  VLayout,
+  VMain,
+  VFooter,
+  VDivider,
+  VSwitch
 } from 'vuetify/components'
 
 export default {
@@ -224,6 +272,7 @@ export default {
     login() {},
     signup() {},
     nextFlow() {},
+    changeTheme() {},
     openDialog() {
       this.dialog = true
     }
@@ -244,7 +293,15 @@ export default {
     VApp,
     VAvatar,
     VImg,
-    VSelect
+    VSelect,
+    VToolbar,
+    VToolbarTitle,
+    VAppBarNavIcon,
+    VLayout,
+    VMain,
+    VFooter,
+    VDivider,
+    VSwitch
   }
 }
 </script>
@@ -274,10 +331,22 @@ export default {
 .text-start {
   text-align: start;
 }
-
 .splash-image {
   width: 140%;
   height: 100%;
   align-content: end;
+}
+.toolbar-text {
+  font-size: 36px;
+  font-display: 'Lato';
+}
+.background-image-column {
+  /* Set the background image */
+  background-image: url('C:\Users\Kumbirai\OneDrive\Documents\GitHub\WorkWise-Central\src\frontend\img\splash.jpg');
+  /* Position the background image to fill the right side */
+  background-position: right;
+  background-size: cover; /* Auto width, full height */
+  background-repeat: no-repeat;
+  max-height: max-content;
 }
 </style>
--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,10 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { createVuetify } from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createVuetify } from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import "vuetify/styles";
 
-const vuetify = createVuetify({})
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 createApp(App)
     .use(router)
     .use(vuetify)
