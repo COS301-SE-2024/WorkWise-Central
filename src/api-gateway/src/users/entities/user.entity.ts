45cd1f7c2d97b2665adc6dfbf6537e0f643a7dbe
Hamza Mokiwa <u22575172@tuks.co.za>
2024-05-29 13:34:55 +0200

    Update user class

--- src/api-gateway/src/users/entities/user.entity.ts
+++ src/api-gateway/src/users/entities/user.entity.ts
@@ -1,9 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Schema()
 export class User {
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   user_UUID: string;
+
+  @Prop({ required: true, unique: true })
+  displayName: string;
 
   @Prop({ required: true })
   name: string;
@@ -11,18 +15,21 @@ export class User {
   @Prop({ required: true })
   surname: string;
 
-  @Prop({ required: false })
+  @Prop({ required: true, unique: true, lowercase: true })
+  email: string;
+
+  @Prop({ required: true })
   password: string;
 
   @Prop({ type: Date, required: true })
   dateOfBirth: Date;
   //dateOfBirth: { type: number; default: DateConstructor['now'] };
 
-  @Prop({ required: true })
-  email: string;
-
   @Prop({ type: [String], required: true })
   roles: string[];
+
+  @Prop({ required: false })
+  profilePicture: string; //Will be a base64 string
 
   @Prop({ required: false })
   public created_at: Date;
@@ -35,3 +42,13 @@ export class User {
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    console.log(this.email, this.password);
+  } catch (error) {
+    next(error);
+  }
+});
