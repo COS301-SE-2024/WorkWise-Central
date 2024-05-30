7dadf5600281963fda477335663d221e97f52c85
Hamza Mokiwa <u22575172@tuks.co.za>
2024-05-30 19:01:55 +0200

    Refactor done, and tested

--- src/api-gateway/src/users/entities/user.entity.ts
+++ src/api-gateway/src/users/entities/user.entity.ts
@@ -1,49 +1,50 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import * as bcrypt from 'bcrypt';
-import { ApiHideProperty } from '@nestjs/swagger';
+//import * as bcrypt from 'bcrypt';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 
 @Schema()
-export class User {
-  @ApiHideProperty()
-  @Prop({ required: true, unique: true })
-  user_UUID: string;
-
-  @ApiHideProperty()
-  @Prop({ required: true, unique: true })
-  displayName: string;
-
-  @ApiHideProperty()
+export class systemDetails {
   @Prop({ required: true })
-  name: string;
-
-  @ApiHideProperty()
-  @Prop({ required: true })
-  surname: string;
-
-  @ApiHideProperty()
-  @Prop({ required: true, unique: true, lowercase: true })
   email: string;
-
-  @ApiHideProperty()
-  @Prop({ required: true })
+  @Prop({ required: true, unique: false })
   password: string;
+  @Prop({ required: true, unique: true })
+  username: string;
+}
+@Schema()
+export class contactInfo {
+  @Prop({ type: String, required: true })
+  phoneNumber: string;
+
+  @Prop({ type: String, required: true })
+  email: string;
+}
+@Schema()
+export class address {
+  @Prop({ type: String, required: false })
+  street: string;
+  @Prop({ type: String, required: false })
+  suburb: string;
+  @Prop({ type: String, required: false })
+  city: string;
+  @Prop({ type: String, required: false })
+  postalCode: string;
+  @Prop({ type: String, required: false })
+  complex: string;
+  @Prop({ type: String, required: false })
+  houseNumber: string;
+}
+@Schema()
+export class personalInfo {
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true, unique: false })
+  surname: string;
 
   @ApiHideProperty()
   @Prop({ type: Date, required: true })
   dateOfBirth: Date;
-  //dateOfBirth: { type: number; default: DateConstructor['now'] };
-
-  @ApiHideProperty()
-  @Prop({ type: [String], required: true })
-  roles: string[];
-
-  @ApiHideProperty()
-  @Prop({
-    required: false,
-    default:
-      'https://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=mp',
-  })
-  profilePicture: string; //Will be a base64 string
 
   @ApiHideProperty()
   @Prop({ required: true, default: 'Rather Not Say' })
@@ -52,6 +53,58 @@ export class User {
   @ApiHideProperty()
   @Prop({ required: false, default: 'English' })
   preferred_Language: string;
+
+  @ApiHideProperty()
+  @Prop({ type: contactInfo, required: false })
+  contactInfo: contactInfo;
+
+  @ApiHideProperty()
+  @Prop({ type: address, required: false })
+  address: address;
+}
+
+@Schema()
+export class profile {
+  @Prop({ type: String, required: true })
+  displayName: string;
+
+  @Prop({
+    required: false,
+    default:
+      'https://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=mp',
+  })
+  displayImage: string;
+}
+@Schema()
+export class roles {
+  @Prop({ type: String, required: false })
+  role: string;
+
+  @Prop({ type: [String], required: false })
+  permissions: string[];
+}
+
+@Schema()
+export class User {
+  @ApiProperty()
+  @Prop({ required: true, unique: true })
+  user_UUID: string;
+
+  @ApiProperty()
+  @Prop({ required: true })
+  systemDetails: systemDetails;
+
+  @ApiProperty()
+  @Prop({ required: true })
+  personalInfo: personalInfo;
+
+  @ApiProperty()
+  @Prop({ required: true })
+  profile: profile;
+
+  @ApiProperty()
+  @Prop({ required: false })
+  roles: roles;
 
   @ApiHideProperty()
   @Prop({ required: false, default: new Date() })
@@ -68,7 +121,7 @@ export class User {
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
-UserSchema.pre('save', async function (next) {
+/*UserSchema.pre('save', async function (next) {
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -76,4 +129,4 @@ UserSchema.pre('save', async function (next) {
   } catch (error) {
     next(error);
   }
-});
+});*/
