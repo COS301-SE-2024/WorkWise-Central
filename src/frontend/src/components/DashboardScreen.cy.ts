96a8179777c7edd6eb70dfbf847baa216b545629
JessicaBloem <jessicabloem8@gmail.com>
2024-06-02 19:41:00 +0200

    feat: update DashboardScreen test

--- src/frontend/src/components/DashboardScreen.cy.ts
+++ src/frontend/src/components/DashboardScreen.cy.ts
@@ -2,12 +2,11 @@ import { createVuetify } from 'vuetify'
 import { mount } from 'cypress/vue'
 import Modals from '../views/Modals.vue'
 
-it('shows the register company modal', () => {
+it('render dashboard', () => {
   const vuetify = createVuetify()
   mount(Modals, {
     global: {
       plugins: [vuetify]
     }
   })
-  
 })
