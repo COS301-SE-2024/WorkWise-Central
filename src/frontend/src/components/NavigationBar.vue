1b465a11c1ac409a09bfecd518967674f6daa96e
Thando Dlamini <u22906160@tuks.co.za>
2024-06-07 21:02:48 +0200

    Update NavigationBar.vue

--- src/frontend/src/components/NavigationBar.vue
+++ src/frontend/src/components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 import '@mdi/font/css/materialdesignicons.css'; // icon import
 
 const isVisible = ref(false);
