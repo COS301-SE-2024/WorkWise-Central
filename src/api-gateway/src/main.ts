ead9684c93e364928fa33620b67e894cbf5b7a0d
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-08 15:20:12 +0200

    feat: Improves type-checking and enables class validation

--- src/api-gateway/src/main.ts
+++ src/api-gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000);
 }
