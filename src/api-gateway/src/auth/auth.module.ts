875d2723b019c2bcd11a144e623494edcfe67b0c
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-11 18:09:14 +0200

    feat: imports email module and makes auth global

--- src/api-gateway/src/auth/auth.module.ts
+++ src/api-gateway/src/auth/auth.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { jwtConstants } from './constants';
 import { JwtModule } from '@nestjs/jwt';
+import { EmailModule } from '../email/email.module';
 
+@Global()
 @Module({
   imports: [
     UsersModule,
@@ -13,6 +15,7 @@ import { JwtModule } from '@nestjs/jwt';
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '10h' },
     }),
+    EmailModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
