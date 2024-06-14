a61d1daa59ed9eb01439cc1ba786a1cfc892d9aa
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-14 23:37:23 +0200

    fix: circular dependancy

--- src/api-gateway/src/auth/auth.module.ts
+++ src/api-gateway/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
@@ -7,11 +7,11 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    UsersModule,
+    forwardRef(() => UsersModule),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '10h' },
+      signOptions: { expiresIn: '1333333h' },
     }),
   ],
   controllers: [AuthController],
--- src/api-gateway/src/users/users.module.ts
+++ src/api-gateway/src/users/users.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
 import { UsersController } from './users.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
+    forwardRef(() => AuthModule),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ], //'user' is the collection name
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService, MongooseModule],
