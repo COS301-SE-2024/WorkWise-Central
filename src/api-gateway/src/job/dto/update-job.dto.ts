0f9af52fc0caf10da69bd588757f562c2ccdff68
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-15 17:57:28 +0200

    feat: Defined nested createJob attributes

--- src/api-gateway/src/job/dto/update-job.dto.ts
+++ src/api-gateway/src/job/dto/update-job.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/swagger';
-import { Job } from '../entities/job.entity';
+import {
+  AssignedEmployees,
+  ClientFeedback,
+  Details,
+  Job,
+  RecordedDetails,
+  Task,
+  Comment,
+} from '../entities/job.entity';
+
+class UpdateAssignedEmployees extends PartialType(AssignedEmployees) {}
+class UpdateDetails extends PartialType(Details) {}
+class UpdateRecordedDetails extends PartialType(RecordedDetails) {}
+class UpdateClientFeedback extends PartialType(ClientFeedback) {}
+class UpdateTask extends PartialType(Task) {}
+class UpdateComment extends PartialType(Comment) {}
 
 export class UpdateJobDto extends PartialType(Job) {}
