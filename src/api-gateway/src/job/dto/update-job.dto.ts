0ab7addb4a5aa1b89541e3b3b166ce1e42a012c2
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-15 18:02:25 +0200

    feat: Defined updateJobDto attributes

--- src/api-gateway/src/job/dto/update-job.dto.ts
+++ src/api-gateway/src/job/dto/update-job.dto.ts
@@ -1,13 +1,20 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import {
   AssignedEmployees,
   ClientFeedback,
   Details,
-  Job,
   RecordedDetails,
   Task,
   Comment,
 } from '../entities/job.entity';
+import {
+  IsMongoId,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Types } from 'mongoose';
+import { Type } from 'class-transformer';
 
 class UpdateAssignedEmployees extends PartialType(AssignedEmployees) {}
 class UpdateDetails extends PartialType(Details) {}
@@ -16,4 +23,64 @@ class UpdateClientFeedback extends PartialType(ClientFeedback) {}
 class UpdateTask extends PartialType(Task) {}
 class UpdateComment extends PartialType(Comment) {}
 
-export class UpdateJobDto extends PartialType(Job) {}
+export class UpdateJobDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsMongoId()
+  companyId?: Types.ObjectId;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsMongoId()
+  clientId?: Types.ObjectId;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  clientUsername?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  assignedBy?: Types.ObjectId;
+
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => UpdateAssignedEmployees)
+  @IsOptional()
+  assignedEmployees?: UpdateAssignedEmployees;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  status?: string;
+
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => UpdateDetails)
+  @IsOptional()
+  details?: UpdateDetails;
+
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => UpdateRecordedDetails)
+  @IsOptional()
+  recordedDetails?: UpdateRecordedDetails;
+
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => UpdateClientFeedback)
+  @IsOptional()
+  clientFeedback?: UpdateClientFeedback;
+
+  @ApiProperty()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateTask)
+  @IsOptional()
+  taskList?: UpdateTask[];
+
+  @ApiProperty()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateComment)
+  @IsOptional()
+  comments?: UpdateComment[];
+}
