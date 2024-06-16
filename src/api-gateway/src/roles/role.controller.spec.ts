99fef91e8b6d0ef886fc701c175d2bee94341869
JessicaBloem <jessicabloem8@gmail.com>
2024-06-16 16:23:12 +0200

    refactor(employee): added role entity

---
+++ src/api-gateway/src/roles/role.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EmployeeController } from './role.controller';
+import { EmployeeService } from './role.service';
+
+describe('EmployeeController', () => {
+  let controller: EmployeeController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [EmployeeController],
+      providers: [EmployeeService],
+    }).compile();
+
+    controller = module.get<EmployeeController>(EmployeeController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
