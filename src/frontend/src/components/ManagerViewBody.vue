bee07096d449505931a5bc02f1b5c80da6554f93
bob dlm <118727166+CodeHermez@users.noreply.github.com>
2024-06-17 10:34:36 +0200

    changed color scheme of manager employees view

--- src/frontend/src/components/ManagerViewBody.vue
+++ src/frontend/src/components/ManagerViewBody.vue
@@ -40,26 +40,30 @@ export default defineComponent({
               single-line
             ></v-text-field>
             <v-spacer></v-spacer>
-            <v-col><AddEmployee></AddEmployee></v-col>
+            <v-col cols="2" class="justify-lg-end"><AddEmployee></AddEmployee></v-col>
           </v-card-title>
         </v-col>
         <v-col class="search_bar_space"> </v-col>
         <v-spacer></v-spacer>
 
         <v-col>
-          <v-sheet rounded class="h-lg-screen w-lg-screen" color="red">
+          <v-sheet rounded class="h-lg-screen w-lg-screen">
             <v-row v-for="i in 10" :key="i">
               <v-col :cols="12">
-                <v-card color="#879898" :height="50" rounded="xl">
+                <v-card color="rgb(135,152,152,0.40)" :height="50" rounded>
                   <v-row class="center">
-                    <v-col :cols="2"><v-card class="pa-2 ma-1"> Tinashe Fadza</v-card></v-col>
+                    <v-col cols="auto"
+                      ><v-card class="pa-2 ma-1" color="rgb(135,152,152,0)" variant="flat">
+                        Tinashe Fadza</v-card
+                      ></v-col
+                    >
                     <v-col :cols="1">
-                      <v-card class="pa-2 ma-1">
+                      <v-card class="pa-3" color="rgb(135,152,152,0)" variant="flat">
                         <v-row justify="center">
                           <v-menu min-width="200px" rounded>
                             <template v-slot:activator="{ props }">
-                              <v-btn icon v-bind="props">
-                                <v-avatar color="brown" size="large">
+                              <v-btn icon v-bind="props" variant="flat">
+                                <v-avatar color="red" size="large">
                                   <span class="text-h5">{{ 'TF' }}</span>
                                 </v-avatar>
                               </v-btn>
@@ -67,7 +71,7 @@ export default defineComponent({
                             <v-card>
                               <v-card-text>
                                 <div class="mx-auto text-center">
-                                  <v-avatar color="brown">
+                                  <v-avatar color="blue">
                                     <span class="text-h5">{{ 'TF' }}</span>
                                   </v-avatar>
                                   <h3>{{ 'Tinas' }}</h3>
@@ -77,7 +81,7 @@ export default defineComponent({
                                   <v-divider class="my-3"></v-divider>
                                   <v-btn variant="text" rounded> Edit Account </v-btn>
                                   <v-divider class="my-3"></v-divider>
-                                  <v-btn variant="text" rounded> Disconnect </v-btn>
+                                  <v-btn variant="text" rounded> Remove Account </v-btn>
                                 </div>
                               </v-card-text>
                             </v-card>
@@ -86,28 +90,37 @@ export default defineComponent({
                       </v-card></v-col
                     >
                     <v-col :cols="6" align="center">
-                      <v-card class="pa-2 ma-1">dummy@gmail.com</v-card></v-col
+                      <v-card class="pa-2 ma-1" color="rgb(135,152,152,0)" variant="flat"
+                        >dummy@gmail.com</v-card
+                      ></v-col
                     >
                     <v-col :cols="3" align="end">
-                      <v-card class="pa-2 ma-1 justify-lg-end"
-                        ><v-btn color="yellow"> ASSIGN </v-btn></v-card
+                      <v-card
+                        class="pa-1 ma-1 justify-lg-end"
+                        color="rgb(135,152,152,0)"
+                        variant="flat"
+                        ><v-btn color="#879898" variant="flat"> ASSIGN </v-btn></v-card
                       ></v-col
                     >
                   </v-row>
                 </v-card>
               </v-col>
               <v-col :cols="12">
-                <v-card color="#6A99CE" :height="50" rounded="xl" class="justify-center">
+                <v-card color="#6A99CE" :height="50" rounded class="justify-center">
                   <v-row class="center">
-                    <v-col :cols="2"><v-card class="pa-2 ma-1"> Tinashe Fadza</v-card></v-col>
+                    <v-col cols="auto"
+                      ><v-card class="pa-2 ma-1" color="#6A99CE" variant="flat">
+                        Tinashe Fadza</v-card
+                      ></v-col
+                    >
                     <v-col :cols="1">
-                      <v-card class="pa-2 ma-1">
+                      <v-card class="pa-3" color="#6A99CE" variant="flat">
                         <!--THIS IS WHERE THE ICON IS PLACED-->
                         <v-row justify="center">
                           <v-menu min-width="200px" rounded>
                             <template v-slot:activator="{ props }">
-                              <v-btn icon v-bind="props">
-                                <v-avatar color="brown" size="large">
+                              <v-btn variant="flat" icon v-bind="props">
+                                <v-avatar color="#879898" size="large">
                                   <span class="text-h5">{{ 'GR' }}</span>
                                 </v-avatar>
                               </v-btn>
@@ -115,7 +128,7 @@ export default defineComponent({
                             <v-card>
                               <v-card-text>
                                 <div class="mx-auto text-center">
-                                  <v-avatar color="brown">
+                                  <v-avatar color="#879898">
                                     <span class="text-h5">{{ 'GR' }}</span>
                                   </v-avatar>
                                   <h3>{{ 'Tinas' }}</h3>
@@ -125,7 +138,7 @@ export default defineComponent({
                                   <v-divider class="my-3"></v-divider>
                                   <v-btn variant="text" rounded> Edit Account </v-btn>
                                   <v-divider class="my-3"></v-divider>
-                                  <v-btn variant="text" rounded> Disconnect </v-btn>
+                                  <v-btn variant="text" rounded> Remove Account </v-btn>
                                 </div>
                               </v-card-text>
                             </v-card>
@@ -135,11 +148,13 @@ export default defineComponent({
                       </v-card></v-col
                     >
                     <v-col :cols="6" align="center">
-                      <v-card class="pa-2 ma-1">dummy@gmail.com</v-card></v-col
+                      <v-card class="pa-2 ma-1" color="#6A99CE" variant="flat"
+                        >dummy@gmail.com</v-card
+                      ></v-col
                     >
                     <v-col :cols="3" align="end">
-                      <v-card class="pa-2 ma-1 justify-lg-end"
-                        ><v-btn color="yellow"> ASSIGN </v-btn></v-card
+                      <v-card class="pa-1 ma-1 justify-lg-end" color="#6A99CE" variant="flat"
+                        ><v-btn color="#879898" variant="flat"> ASSIGN </v-btn></v-card
                       ></v-col
                     >
                   </v-row>
