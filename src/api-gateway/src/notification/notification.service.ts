35c9c3131348b8a75737786f77b8be5c3bd472e7
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-17 22:08:28 +0200

    feat: notificationService watches DB

--- src/api-gateway/src/notification/notification.service.ts
+++ src/api-gateway/src/notification/notification.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
+import * as console from 'node:console';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class NotificationService {
+export class NotificationService implements OnModuleInit {
+  constructor(
+    @InjectModel('Notification')
+    private readonly notificationModel: Model<Notification>,
+  ) {}
+
+  onModuleInit() {
+    this.watchDatabase();
+  }
+
   create(createNotificationDto: CreateNotificationDto) {
     console.log(createNotificationDto);
     return 'This action adds a new notification';
+  }
+
+  watchDatabase() {
+    this.notificationModel.watch().on('change', (change) => {
+      console.log(change);
+    });
   }
 
   findAll() {
