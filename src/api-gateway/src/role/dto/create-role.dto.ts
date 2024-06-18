edbbff72c0727c926e8a562ee78a672757b3b2eb
JessicaBloem <jessicabloem8@gmail.com>
2024-06-18 06:46:54 +0200

    feature: added create-role.dto

---
+++ src/api-gateway/src/role/dto/create-role.dto.ts
@@ -0,0 +1,56 @@
+import { Types } from 'mongoose';
+import {
+  IsArray,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateRoleDto {
+  @IsArray()
+  @IsMongoId({ each: true })
+  @IsOptional()
+  @ApiProperty()
+  roleId: Types.ObjectId;
+
+  // @IsArray()
+  // @IsMongoId({ each: true })
+  // @ApiProperty()
+  // currentJobAssignments: Types.ObjectId[];
+   
+  @IsMongoId()
+  @IsOptional()
+  @ApiProperty()
+  superiorId: Types.ObjectId;
+
+  // @IsArray()
+  // @IsMongoId({ each: true })
+  // @IsOptional()
+  // @ApiProperty()
+  // subordinates: Types.ObjectId[];
+
+  // @IsArray()
+  // @IsMongoId({ each: true })
+  // @IsOptional()
+  // @ApiProperty()
+  // subordinateRoles: Types.ObjectId[];
+
+  @IsNotEmpty()
+  @IsMongoId()
+  @ApiProperty()
+  userId: Types.ObjectId;
+
+  @IsNotEmpty()
+  @IsMongoId()
+  @ApiProperty()
+  companyId: Types.ObjectId;
+
+}
+
+export class createRoleResponseDto{
+  response: { access_token: string; id: Types.ObjectId };
+  constructor(message: { access_token: string; id: Types.ObjectId }) {
+    this.response = message;
+  }
+}
