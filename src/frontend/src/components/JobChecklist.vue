f6ccb59184f9ce377a71bebeb12d44ab36a2daa9
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-06-21 03:27:43 +0200

    feature: completed the basic setup of the job assignment card
    Added three components for the job assignment card
    Team Members List
    Labels
    Checklist

---
+++ src/frontend/src/components/JobChecklist.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-dialog v-model="checklistDialog" max-width="500" height="auto">
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn
+        rounded="md"
+        class="text-none font-weight-regular hello"
+        prepend-icon="mdi-label"
+        variant="elevated"
+        v-bind="activatorProps"
+      >
+        Checklist
+      </v-btn>
+    </template>
+    <v-sheet elevation="14" rounded="xl" width="500" height="auto">
+      <v-col>
+        <v-row class="pa-2" align="center">
+          <!-- Add padding for spacing and align items center -->
+
+          <!-- Label in the middle -->
+          <v-col cols="10">
+            <h4 class="text-center" style="font-size: 25px; font-weight: lighter">Add Checklist</h4>
+          </v-col>
+
+          <!-- Close button on the right -->
+          <v-col cols="2">
+            <v-btn @click="editLabelDialog = false" variant="plain">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-spacer></v-spacer>
+
+        <v-col cols="12"
+          ><v-label>Title</v-label>
+          <v-text-field
+            v-model="checklist"
+            :label="checklist ? checklist : 'Checklist'"
+            variant="outlined"
+            hide-details
+            width="95%"
+            border="md"
+            density="compact"
+          ></v-text-field> </v-col
+        ><v-spacer></v-spacer>
+        <v-col cols="12"><v-btn variant="elevated" color="blue">Add</v-btn></v-col>
+      </v-col>
+    </v-sheet>
+  </v-dialog>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'JobChecklist',
+  data: () => ({
+    checklistDialog: false,
+    checklist: ''
+  })
+})
+</script>
