84bcaf9816c2f499ee7929eccceb35b0444b22ba
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-23 12:33:14 +0200

    fix: Fixed role controller test and removed team test placeholders

--- src/api-gateway/src/role/role.controller.spec.ts
+++ src/api-gateway/src/role/role.controller.spec.ts
@@ -1,14 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoleController } from './role.controller';
 import { RoleService } from './role.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Role } from './entity/role.entity';
 
 describe('RoleController', () => {
   let controller: RoleController;
 
+  class MockRoleModel {
+    constructor(private data: any) {}
+    create = jest.fn().mockResolvedValue(this.data);
+    static find = jest.fn().mockResolvedValue(['event']);
+    static findOne = jest.fn().mockResolvedValue('event');
+    static findOneAndUpdate = jest.fn().mockResolvedValue('event');
+    static deleteOne = jest.fn().mockResolvedValue(true);
+  }
+
+  class MockRoleService {
+    verifyUser(a?: any) {
+      return !!a;
+    }
+    exists(a?: any) {
+      return a != 'b';
+    }
+    softDelete(a: any) {
+      console.log(a);
+    }
+    getUserByUsername() {
+      return null;
+    }
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RoleController],
-      providers: [RoleService],
+      providers: [
+        { provide: RoleService, useClass: MockRoleService },
+        { provide: getModelToken(Role.name), useValue: MockRoleModel },
+      ],
     }).compile();
 
     controller = module.get<RoleController>(RoleController);
