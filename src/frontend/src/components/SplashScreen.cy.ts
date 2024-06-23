d9ebff1096866cc1d790ae1aac89da5dd815816d
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-06-23 12:02:48 +0200

    fix: resolved the issue of tests not running

--- src/frontend/src/components/SplashScreen.cy.ts
+++ src/frontend/src/components/SplashScreen.cy.ts
@@ -2,35 +2,54 @@ import { createVuetify } from 'vuetify'
 import { mount } from 'cypress/vue'
 import SplashScreen from '../components/SplashScreen.vue'
 
-beforeEach(() => {
+
+it('shows the splash screen', () => {
   const vuetify = createVuetify()
   mount(SplashScreen, {
     global: {
       plugins: [vuetify]
     }
   })
-})
-
-it('shows the splash screen', () => {
   cy.contains('WorkWise')
   cy.contains('Welcome To WorkWise Central')
 })
 
 it('performs login', () => {
-  cy.contains('button', 'Log in').click()
+  const vuetify = createVuetify()
+  mount(SplashScreen, {
+    global: {
+      plugins: [vuetify]
+    }
+  })
+  cy.contains('button', 'Log in').click() // Clicks the button containing the text 'Log in'
+
   cy.get('input[name="username"]').type('test')
   cy.get('input[name="password"]').type('test')
-  // Ensure this matches your button's text
-  cy.contains('button', 'LOGIN').click()
+ // cy.contains('button', 'LOGIN').click() // Clicks the button containing the text 'Log in'
 })
 
 it('performs register', () => {
-  cy.contains('button', 'Sign up').click()
+  const vuetify = createVuetify()
+  mount(SplashScreen, {
+    global: {
+      plugins: [vuetify]
+    }
+  })
+  cy.contains('button', 'Sign up').click() // Clicks the button containing the text 'Sign up'
   cy.get('input[type="email"]').type('test@test.com')
   cy.get('input[name="password"]').type('Test123#')
   cy.get('input[name="confirm_password"]').type('Test123#')
+
 })
 
-it('performs theme change', () => {
-  cy.contains('button', 'Theme').click()
+it('performs theme change',()=>{
+    const vuetify = createVuetify()
+    mount(SplashScreen, {
+        global: {
+        plugins: [vuetify]
+        }
+    })
+    cy.contains('button', 'Theme').click() // Clicks the button containing the text 'Change Theme'
+    
+   
 })
