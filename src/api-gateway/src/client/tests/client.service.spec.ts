2d655c3717869469d3acea5944325344a33dcddb
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-23 15:35:21 +0200

    fix: Tests updated

--- src/api-gateway/src/client/tests/client.service.spec.ts
+++ src/api-gateway/src/client/tests/client.service.spec.ts
@@ -2,6 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ClientService } from '../client.service';
 import { userStub } from '../../../test/stubs/user.stub';
 import { ClientRepository } from '../client.repository';
+import { CompanyService } from '../../company/company.service';
+import { UsersService } from '../../users/users.service';
+import { MockFunctionMetadata, ModuleMocker } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
+
+class MockJobModel {
+  constructor(private data: any) {}
+  create = jest.fn().mockResolvedValue(this.data);
+  static find = jest.fn().mockResolvedValue(['event']);
+  static watch = jest.fn().mockResolvedValue(['event']);
+  static findOne = jest.fn().mockResolvedValue('event');
+  static findOneAndUpdate = jest.fn().mockResolvedValue('event');
+  static deleteOne = jest.fn().mockResolvedValue(true);
+}
 
 describe('ClientService', () => {
   let service: ClientService;
@@ -18,6 +33,7 @@ describe('ClientService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ClientService,
+        { provide: CompanyService, useClass: MockJobModel },
         {
           provide: 'ClientModel',
           useValue: mockClientModel,
@@ -27,7 +43,44 @@ describe('ClientService', () => {
           useValue: mockClientModel,
         },
       ],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === UsersService) {
+          return {
+            create: jest.fn().mockReturnValue(userStub()),
+            findAllUsers: jest.fn().mockReturnValue(userStub()),
+            findUser: jest.fn().mockReturnValue(userStub()),
+            update: jest.fn().mockReturnValue(userStub()),
+            remove: jest.fn().mockReturnValue(userStub()),
+          };
+        }
+        if (token === CompanyService) {
+          return {
+            create: jest.fn().mockReturnValue(userStub()),
+            findAllUsers: jest.fn().mockReturnValue(userStub()),
+            findUser: jest.fn().mockReturnValue(userStub()),
+            update: jest.fn().mockReturnValue(userStub()),
+            remove: jest.fn().mockReturnValue(userStub()),
+          };
+        }
+        if (token === ClientService) {
+          return {
+            create: jest.fn().mockReturnValue(userStub()),
+            findAllUsers: jest.fn().mockReturnValue(userStub()),
+            findUser: jest.fn().mockReturnValue(userStub()),
+            update: jest.fn().mockReturnValue(userStub()),
+            remove: jest.fn().mockReturnValue(userStub()),
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     service = module.get<ClientService>(ClientService);
   });
