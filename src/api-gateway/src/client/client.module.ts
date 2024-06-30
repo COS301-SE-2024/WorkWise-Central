8279a2f91c614d43cdfc14b4819594acf71d7e4b
Hamza Mokiwa <u22575172@tuks.co.za>
2024-06-30 18:46:18 +0200

    feat: createCompany now adds user as employee

--- src/api-gateway/src/client/client.module.ts
+++ src/api-gateway/src/client/client.module.ts
@@ -7,12 +7,14 @@ import { ClientRepository } from './client.repository';
 import { CompanyService } from '../company/company.service';
 import { CompanyModule } from '../company/company.module';
 import { EmployeeModule } from '../employee/employee.module';
+import { RoleModule } from '../role/role.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]),
     forwardRef(() => CompanyModule),
     forwardRef(() => EmployeeModule),
+    forwardRef(() => RoleModule),
   ],
   controllers: [ClientController],
   providers: [ClientService, ClientRepository, CompanyService],
