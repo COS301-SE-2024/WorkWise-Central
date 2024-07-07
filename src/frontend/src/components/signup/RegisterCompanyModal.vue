a3dde82894a594187aed7fe2bfb1bf5dfe019cbf
bob dlm <118727166+CodeHermez@users.noreply.github.com>
2024-07-07 22:34:17 +0200

    fix: change in theme of modal and change in the shape of text boxes

--- src/frontend/src/components/signup/RegisterCompanyModal.vue
+++ src/frontend/src/components/signup/RegisterCompanyModal.vue
@@ -13,10 +13,10 @@
     </template>
     <v-sheet
       elevation="14"
-      rounded="xl"
+      rounded="md"
       max-width="600"
       max-height="800"
-      :color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+      :color="isdarkmode === true ? 'dark' : 'light'"
     >
       <v-form ref="form" v-model="valid" @submit.prevent="registrationHandler">
         <v-col>
@@ -39,8 +39,8 @@
                 placeholder="Enter the company's name"
                 v-model="req_obj.name"
                 :rules="company_name_rules"
-                rounded="xl"
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                rounded="md"
+                :bg-color="isdarkmode === true ? 'dark' : 'light'"
                 variant="solo"
                 required
               ></v-text-field
@@ -52,12 +52,12 @@
                 >Type of business*</small
               >
               <v-autocomplete
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                :bg-color="isdarkmode === true ? 'dark' : 'light'"
                 density="compact"
                 color="grey-lighten-4"
                 placeholder="Select Job Type"
                 v-model="req_obj.type"
-                rounded="xl"
+                rounded="md"
                 variant="solo"
                 :items="[
                   'Agricultural Labor',
@@ -141,14 +141,14 @@
                 >Company email address</small
               >
               <v-text-field
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                :bg-color="isdarkmode === true ? 'dark' : 'light'"
                 density="compact"
                 color="grey-lighten-4"
                 placeholder="Enter the company's email adress"
                 type="email"
                 :rules="email_rules"
                 v-model="req_obj.contactDetails.email"
-                rounded="xl"
+                rounded="md"
                 variant="solo"
                 required
               ></v-text-field
@@ -160,11 +160,11 @@
                 >Company phone number</small
               >
               <v-text-field
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                :bg-color="isdarkmode === true ? 'dark' : 'light'"
                 density="compact"
                 color="grey-lighten-4"
                 placeholder="Enter the company's phone number"
-                rounded="xl"
+                rounded="md"
                 variant="solo"
                 v-model="req_obj.contactDetails.phoneNumber"
                 required
@@ -177,13 +177,13 @@
                 >Company registration number</small
               >
               <v-text-field
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                :bg-color="isdarkmode === true ? 'dark' : 'light'"
                 density="compact"
                 color="grey-lighten-4"
                 placeholder="Enter the company's registration number"
                 v-model="req_obj.registrationNumber"
                 :rules="company_registration_number_rules"
-                rounded="xl"
+                rounded="md"
                 variant="solo"
                 required
               ></v-text-field
@@ -196,12 +196,12 @@
                 >Company VAT number</small
               >
               <v-text-field
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                :bg-color="isdarkmode === true ? 'dark' : 'light'"
                 density="compact"
                 color="grey-lighten-4"
                 placeholder="Enter the company's VAT number"
                 :rules="vat_number_rules"
-                rounded="xl"
+                rounded="md"
                 v-model="req_obj.vatNumber"
                 variant="solo"
                 required
@@ -214,14 +214,14 @@
             <!--                >Company logo</small-->
             <!--              >-->
             <!--              <v-file-input-->
-            <!--                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"-->
+            <!--                :bg-color="isdarkmode === true ? 'dark' : 'light'"-->
             <!--                variant="solo"-->
             <!--                accept="image/*"-->
             <!--                width="100%"-->
             <!--                placeholder="Company Logo"-->
             <!--                @change="companyLogoHandler"-->
             <!--                color="black"-->
-            <!--                rounded="xl"-->
+            <!--                rounded="md"-->
             <!--              ></v-file-input>-->
             <!--            </v-col>-->
             <small
@@ -236,11 +236,11 @@
                   class="text-caption"
                   >Street</small
                 ><v-text-field
-                  :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                  :bg-color="isdarkmode === true ? 'dark' : 'light'"
                   density="compact"
                   color="grey-lighten-4"
                   placeholder="Street"
-                  rounded="xl"
+                  rounded="md"
                   v-model="req_obj.address.street"
                   variant="solo"
                   required
@@ -252,11 +252,11 @@
                   class="text-caption"
                   >Suburb</small
                 ><v-text-field
-                  :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                  :bg-color="isdarkmode === true ? 'dark' : 'light'"
                   density="compact"
                   color="grey-lighten-4"
                   placeholder="Suburb"
-                  rounded="xl"
+                  rounded="md"
                   v-model="req_obj.address.suburb"
                   variant="solo"
                   required
@@ -268,11 +268,11 @@
                   class="text-caption"
                   >City</small
                 ><v-text-field
-                  :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                  :bg-color="isdarkmode === true ? 'dark' : 'light'"
                   density="compact"
                   color="grey-lighten-4"
                   placeholder="City"
-                  rounded="xl"
+                  rounded="md"
                   v-model="req_obj.address.city"
                   variant="solo"
                   required
@@ -284,11 +284,11 @@
                   class="text-caption"
                   >Zip Code</small
                 ><v-text-field
-                  :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
+                  :bg-color="isdarkmode === true ? 'dark' : 'light'"
                   density="compact"
                   color="grey-lighten-4"
                   placeholder="Zip Code"
-                  rounded="xl"
+                  rounded="md"
                   v-model="req_obj.address.postalCode"
                   variant="solo"
                   required
@@ -298,11 +298,11 @@
             <!--            <v-row>-->
             <!--              <v-col-->
             <!--                ><v-text-field-->
-            <!--                  :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"-->
+            <!--                  :bg-color="isdarkmode === true ? 'dark' : 'light'"-->
             <!--                  density="compact"-->
             <!--                  color="grey-lighten-4"-->
             <!--                  placeholder="Complex"-->
-            <!--                  rounded="xl"-->
+            <!--                  rounded="md"-->
             <!--                  v-model="req_obj.address.complex"-->
             <!--                  variant="solo"-->
             <!--                  required-->
@@ -310,11 +310,11 @@
             <!--              ></v-col>-->
             <!--              <v-col-->
             <!--                ><v-text-field-->
-            <!--                  :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"-->
+            <!--                  :bg-color="isdarkmode === true ? 'dark' : 'light'"-->
             <!--                  density="compact"-->
             <!--                  color="grey-lighten-4"-->
             <!--                  placeholder="House number"-->
-            <!--                  rounded="xl"-->
+            <!--                  rounded="md"-->
             <!--                  v-model="req_obj.address.houseNumber"-->
             <!--                  variant="solo"-->
             <!--                  required-->
@@ -323,7 +323,7 @@
             <!--            </v-row>-->
           </v-col>
           <v-col
-            :style="isdarkmode === true ? dark_theme_text_color : modal_light_theme_color"
+            :style="isdarkmode === true ? dark_theme_text_color : 'light'"
             cols="8"
             offset="2"
             align="center"
@@ -331,8 +331,8 @@
             <v-btn
               color="#5A82AF"
               type="submit"
-              rounded="xl"
-              boarder="xl"
+              rounded="md"
+              boarder="md"
               width="60%"
               height="35"
               variant="elevated"
@@ -354,13 +354,13 @@ export default {
   data() {
     return {
       dialog: false,
-      isdarkmode: localStorage['theme'] !== 'false',
+      isdarkmode: sessionStorage['theme'] !== 'false',
       click_create_client: false,
       valid: true,
       light_theme_text_color: 'color: rgb(0, 0, 0); opacity: 65%',
       dark_theme_text_color: 'color: #DCDBDB',
-      modal_dark_theme_color: '#2b2b2b',
-      modal_light_theme_color: '#FFFFFF',
+      dark: '#2b2b2b',
+      light: '#FFFFFF',
       localUrl: 'http://localhost:3000/',
       remoteUrl: 'https://tuksapi.sharpsoftwaresolutions.net/',
       company_name_rules: [
