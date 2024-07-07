abaa5cc0b12636b9ea805b311a8430cfcc7bb667
bob dlm <118727166+CodeHermez@users.noreply.github.com>
2024-07-07 10:09:35 +0200

    feature: added role selector drop down to change the employees role

--- src/frontend/src/components/home/employees/management/EditEmployee.vue
+++ src/frontend/src/components/home/employees/management/EditEmployee.vue
@@ -25,10 +25,10 @@
                 class="text-caption"
                 :style="isdarkmode === true ? dark_theme_text_color : light_theme_text_color"
               >
-                Address
+                Role
               </small>
               <v-text-field
-                v-model="localEditedItem.address"
+                :v-model="localEditedItem === undefined ? null : localEditedItem.roleName"
                 variant="solo"
                 rounded="md"
               ></v-text-field>
@@ -70,7 +70,7 @@
   </v-dialog>
 </template>
 
-<script>
+<script lang="ts">
 import axios from 'axios'
 export default {
   name: 'EditClient',
@@ -83,26 +83,32 @@ export default {
   },
   data() {
     return {
+      isdarkmode: localStorage['theme'] !== 'false',
       localEditedItem: this.editedItem,
       employeeDialog: false,
       clientName: '', // Assuming you have a way to set this, e.g., when opening the dialog
       isDeleting: false,
-
+      light_theme_text_color: 'color: rgb(0, 0, 0); opacity: 65%',
+      dark_theme_text_color: 'color: #DCDBDB',
+      modal_dark_theme_color: '#2b2b2b',
+      modal_light_theme_color: '#FFFFFF',
+      localUrl: 'http://localhost:3000/',
+      remoteUrl: 'https://tuksapi.sharpsoftwaresolutions.net/',
       nameRules: [
-        (v) => !!v || 'Name is required',
-        (v) => (v && v.length >= 2) || 'Name must be at least 2 characters'
+        (v: string) => !!v || 'Name is required',
+        (v: string) => (v && v.length >= 2) || 'Name must be at least 2 characters'
       ],
       surnameRules: [
-        (v) => !!v || 'Surname is required',
-        (v) => (v && v.length >= 2) || 'Surname must be at least 2 characters'
+        (v: string) => !!v || 'Surname is required',
+        (v: string) => (v && v.length >= 2) || 'Surname must be at least 2 characters'
       ],
       emailRules: [
-        (v) => !!v || 'E-mail is required',
-        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        (v: string) => !!v || 'E-mail is required',
+        (v: string) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
       ],
       phoneRules: [
-        (v) => !!v || 'Phone number is required',
-        (v) => (v && v.length >= 10) || 'Phone number must be at least 10 digits'
+        (v: string) => !!v || 'Phone number is required',
+        (v: string) => (v && v.length >= 10) || 'Phone number must be at least 10 digits'
         // Add more specific validation for phone number format if needed
       ]
     }
@@ -114,6 +120,19 @@ export default {
     }
   },
   methods: {
+    showlcalvalues() {
+      console.log(this.localEditedItem)
+    },
+    async loadRoles() {
+      const config = { headers: { Authorization: `Bearer ${sessionStorage['access_token']}` } }
+      const apiURL = await this.getRequestUrl()
+      console.log(apiURL)
+      try {
+        let role = await axios.get(apiURL + `roles/all/${sessionStorage['currentCompany']}`, config)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    },
     close() {
       this.employeeDialog = false
     },
@@ -127,23 +146,38 @@ export default {
       alert('Client updated')
       this.employeeDialog = false
     },
-    async update() {
-      await axios
-        .patch(`http://localhost:3000/employee/${this.id}`)
-        .then((response) => {
-          console.log(response)
-          alert('Client updated')
-          return true
-        })
-        .catch((error) => {
-          console.log(error)
-          alert('Error updating client')
-          return false
-        })
-        .finally(() => {
-          this.employeeDialog = false
-        })
+    // async update() {
+    //   await axios
+    //     .patch(`http://localhost:3000/employee/${this.id}`)
+    //     .then((response) => {
+    //       console.log(response)
+    //       alert('Client updated')
+    //       return true
+    //     })
+    //     .catch((error) => {
+    //       console.log(error)
+    //       alert('Error updating client')
+    //       return false
+    //     })
+    //     .finally(() => {
+    //       this.employeeDialog = false
+    //     })
+    // },
+    async isLocalAvailable(localUrl: string) {
+      try {
+        const res = await axios.get(localUrl)
+        return res.status < 300 && res.status > 199
+      } catch (error) {
+        return false
+      }
+    },
+    async getRequestUrl() {
+      const localAvailable = await this.isLocalAvailable(this.localUrl)
+      return localAvailable ? this.localUrl : this.remoteUrl
     }
+  },
+  mounted() {
+    this.showlcalvalues()
   }
 }
 </script>
