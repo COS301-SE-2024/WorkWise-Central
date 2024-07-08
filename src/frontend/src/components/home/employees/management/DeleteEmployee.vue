085402ddb8724d5bd22438f0ee5828fc177ed9ad
bob dlm <118727166+CodeHermez@users.noreply.github.com>
2024-07-08 10:04:14 +0200

    fix: delete request works as it should

--- src/frontend/src/components/home/employees/management/DeleteEmployee.vue
+++ src/frontend/src/components/home/employees/management/DeleteEmployee.vue
@@ -10,76 +10,89 @@
         >Delete</v-btn
       >
     </template>
-    <v-card :color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color">
+    <v-card :color="isdarkmode === true ? 'dark' : 'light'">
       <v-card-title>
-        <span class="headline">Delete Client</span>
+        <span class="headline">Delete {{ details.firstName + ' ' + details.surname }}</span>
       </v-card-title>
       <v-card-text>
         <v-container>
           <v-row>
             <p>
-              Are you sure you want to delete <strong>{{ clientName }}</strong
-              >? This action cannot be reversed.
+              Are you sure you want to delete
+              <strong>{{ details.firstName + ' ' + details.surname }}</strong> as an employee of
+              this company?
             </p>
+            <strong> This action cannot be reversed. </strong>
           </v-row>
         </v-container>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="red darken-2" text :loading="isDeleting" @click="Delete">Delete</v-btn>
-        <v-btn color="grey darken-1" text @click="clientDialog = false">Cancel</v-btn>
+        <v-btn color="red darken-2" variant="text" :loading="isDeleting" @click="deleteEmployee"
+          >Delete</v-btn
+        >
+        <v-btn color="grey darken-1" variant="text" @click="clientDialog = false">Cancel</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
-<script>
+<script lang="ts">
 import axios from 'axios'
 export default {
   name: 'DeleteClient',
   props: {
-    opened: Boolean,
-    client_id: Number
+    details: {
+      type: Object,
+      required: true
+    }
   },
   data() {
     return {
       clientDialog: false,
-      clientName: '',
       isDeleting: false,
       isdarkmode: false,
       light_theme_text_color: 'color: rgb(0, 0, 0); opacity: 65%',
       dark_theme_text_color: 'color: #DCDBDB',
       modal_dark_theme_color: '#2b2b2b',
-      modal_light_theme_color: '#FFFFFF'
+      modal_light_theme_color: '#FFFFFF',
+      localUrl: 'http://localhost:3000/',
+      remoteUrl: 'https://tuksapi.sharpsoftwaresolutions.net/'
     }
   },
   methods: {
     close() {
       this.clientDialog = false
     },
-    Delete() {
-      alert('Client deleted')
-      window.location.reload() // Consider removing this for SPA behavior
-    },
-    async deleteClient() {
+    async deleteEmployee() {
       this.isDeleting = true // Indicate the start of the deletion process
+      const config = { headers: { Authorization: `Bearer ${sessionStorage['access_token']}` } }
+      const apiURL = await this.getRequestUrl()
       axios
-        .delete('http://localhost:3000/client/delete', { data: { id: this.client_id } })
+        .delete(apiURL + 'employee/' + this.details.employeeId, config)
         .then((response) => {
           console.log(response)
-          alert('Client deleted')
+          alert('Employee deleted')
+          this.isDeleting = false
           this.clientDialog = false
-          this.$emit('clientDeleted')
-          // Consider using a more SPA-friendly way of updating the view instead of reloading
+          window.location.reload()
         })
         .catch((error) => {
           console.log(error)
           alert('Error deleting client')
         })
-        .finally(() => {
-          this.isDeleting = false // Reset the deletion indicator
-          window.location.reload() // Consider removing this for SPA behavior
-        })
+    },
+    async isLocalAvailable(localUrl: string) {
+      try {
+        const res = await axios.get(localUrl)
+        return res.status < 300 && res.status > 199
+      } catch (error) {
+        return false
+      }
+    },
+    async getRequestUrl() {
+      const localAvailable = await this.isLocalAvailable(this.localUrl)
+      return localAvailable ? this.localUrl : this.remoteUrl
     }
   }
 }
