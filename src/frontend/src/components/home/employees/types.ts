fd1b88033ad8dbe7f2fc6d34f17fefa3b870c20c
bob dlm <118727166+CodeHermez@users.noreply.github.com>
2024-07-11 21:13:55 +0200

    refactor: changed the types in the type files

--- src/frontend/src/components/home/employees/types.ts
+++ src/frontend/src/components/home/employees/types.ts
@@ -1,17 +1,3 @@
-export type Address = {
-  street: string
-  suburb: string
-  city: string
-  postalCode: string
-  complex: string
-  houseNumber: string
-}
-
-export type ContactInfo = {
-  phoneNumber: string
-  email: string
-}
-
 export type Person = {
   address: Address
   contactInfo: ContactInfo
@@ -30,37 +16,6 @@ export type SystemDetails = {
   password: string
   username: string
   _id: string
-}
-
-export type Profile = {
-  displayName: string
-  displayImage: string
-  _id: string
-}
-
-export type Roles = {
-  role: string
-  permissions: string[]
-  _id: string
-}
-
-export type JoinedCompany = {
-  // Define properties for joined companies if there are any; currently it's an empty object
-  // e.g., companyName?: string
-}
-
-export type User = {
-  data: {
-    _id: string
-    systemDetails: SystemDetails
-    personalInfo: EmployeePersonalInfo
-    profile: Profile
-    roles: Roles
-    joinedCompanies: JoinedCompany[]
-    skills: string[]
-    created_at: string // ISO 8601 date string
-    __v: number
-  }
 }
 
 export type JobAssignment = {
@@ -122,4 +77,78 @@ export type EmployeeInformation = {
 export type ClientInformation = {
   name: string
   id: string
+}
+
+type Role = {
+  _id: string
+  roleName: string
+  permissionSuite: string[]
+  companyId: string
+  createdAt: string
+  __v: number
+}
+
+type Address = {
+  street: string
+  suburb: string
+  city: string
+  postalCode: string
+  complex: string
+  houseNumber: string
+}
+
+type ContactInfo = {
+  phoneNumber: string
+  email: string
+}
+
+export type PersonalInfo = {
+  address: Address
+  contactInfo: ContactInfo
+  firstName: string
+  surname: string
+  preferredLanguage: string
+  dateOfBirth: string
+  gender: string
+}
+
+type Profile = {
+  displayName: string
+  displayImage: string
+}
+
+type JoinedCompany = {
+  employeeId: string
+  companyId: string
+  companyName: string
+}
+
+type User = {
+  _id: string
+  systemDetails: {
+    username: string
+    password: string
+  }
+  personalInfo: PersonalInfo
+  profile: Profile
+  joinedCompanies: JoinedCompany[]
+  skills: string[]
+  employeeIds: string[]
+  isValidated: boolean
+  createdAt: string
+  __v: number
+  updatedAt: string
+  currentEmployee: string
+}
+
+export type EmployeeJoined = {
+  _id: string
+  roleId: Role[]
+  currentJobAssignments: any[]
+  subordinates: any[]
+  subordinateTeams: any[]
+  userId: User[]
+  companyId: string
+  createdAt: string
+  __v: number
 }
