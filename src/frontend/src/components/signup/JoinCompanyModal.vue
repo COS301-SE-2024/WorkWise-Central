1b471924256dab23ec898b888c7e1b0b74a0e720
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-07-11 18:26:57 +0200

    fix: updated styling for consistency

--- src/frontend/src/components/signup/JoinCompanyModal.vue
+++ src/frontend/src/components/signup/JoinCompanyModal.vue
@@ -1,24 +1,17 @@
 <template>
-  <v-dialog max-width="500" height="800">
+  <v-dialog v-model="dialog" max-width="500" height="500" :theme="isdarkmode === true ? 'dark' : 'light'">
     <template v-slot:activator="{ props: activatorProps }">
       <v-btn
         rounded="md"
         class="text-none font-weight-regular hello"
-        prepend-icon="mdi-account"
         variant="elevated"
-        color="#5A82AF"
+        color="primary"
         v-bind="activatorProps"
         >Join Company</v-btn
       >
     </template>
-    <v-sheet
-      elevation="14"
-      rounded="xl"
-      width="500"
-      height="800"
-      :color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
-    >
-      <v-form ref="form" v-model="valid" @submit.prevent="handlesubmission">
+    <v-sheet elevation="14" rounded="md" width="500">
+      <v-form ref="form" v-model="valid" @submit.prevent="handlesubmission" class="bg-background">
         <v-col>
           <v-col>
             <h4 class="text-center" style="font-size: 25px; font-weight: lighter">
@@ -28,17 +21,12 @@
           <v-spacer></v-spacer>
           <v-col>
             <v-col>
-              <small
-                class="text-caption"
-                :style="isdarkmode === true ? dark_theme_text_color : light_theme_text_color"
-                >Search for the company by name</small
-              >
+              <small class="text-caption">Search for the company by name</small>
               <v-autocomplete
                 density="compact"
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
                 label="Company Name"
                 variant="solo"
-                rounded="xl"
+                rounded="md"
                 v-model="req_obj.company_name"
                 :items="['Plumber Tronics', 'Nedbank', 'FNB', 'Talker', 'Friends', 'Wyoming']"
               ></v-autocomplete
@@ -49,16 +37,11 @@
               </v-row>
             </v-container>
             <v-col>
-              <small
-                class="text-caption"
-                :style="isdarkmode === true ? dark_theme_text_color : light_theme_text_color"
-                >Enter the Company ID</small
-              >
+              <small class="text-caption">Enter the Company ID</small>
               <v-text-field
                 density="compact"
-                :bg-color="isdarkmode === true ? modal_dark_theme_color : modal_light_theme_color"
                 label="Enter the company ID"
-                rounded="xl"
+                rounded="md"
                 variant="solo"
                 v-model="req_obj.companyID"
                 required
@@ -67,14 +50,26 @@
           </v-col>
           <v-col cols="8" offset="2" align="center">
             <v-btn
-              color="#5A82AF"
-              rounded="xl"
+              color="primary"
+              rounded="md"
               boarder="xl"
               width="85%"
               height="35"
               variant="elevated"
               :disabled="req_obj.company_name === '' && req_obj.companyID === ''"
-              >JOIN COMPANY</v-btn
+              >Join Company</v-btn
+            >
+          </v-col>
+          <v-col cols="8" offset="2" align="center">
+            <v-btn
+              color="secondary"
+              @click="close"
+              rounded="md"
+              boarder="xl"
+              width="85%"
+              height="35"
+              variant="elevated"
+              >Back</v-btn
             >
           </v-col>
         </v-col>
@@ -88,7 +83,6 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'RegisterCompanyModal',
 
-  props: ['isdarkmode'],
   data: () => ({
     dialog: false,
     valid: false,
@@ -98,7 +92,7 @@ export default defineComponent({
     modal_light_theme_color: '#FFFFFF',
     click_create_client: false,
     attribute_is_filled: false,
-
+    isdarkmode: sessionStorage.getItem('theme') === 'true' ? true : false,
     req_obj: {
       company_name: '',
       companyID: ''
@@ -108,6 +102,9 @@ export default defineComponent({
   methods: {
     handlesubmission() {
       alert('Added employee successfully')
+    },
+    close() {
+      this.dialog = false
     }
   }
 })
