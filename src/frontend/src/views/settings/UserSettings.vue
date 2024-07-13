e6e97a3b78e50ce4a759622703c73388f93a9707
Thando Dlamini <u22906160@tuks.co.za>
2024-07-13 12:17:08 +0200

    src: Adds user settings

--- src/frontend/src/views/settings/UserSettings.vue
+++ src/frontend/src/views/settings/UserSettings.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import NavigationBar from '../../components/home/NavigationBar.vue'
+import UserPage from '../../components/home/settings/user/UserPage.vue'
 </script>
 
 <template>
-  <NavigationBar />
+  <NavigationBar :component="UserPage"/>
 </template>
 
 <style scoped>
