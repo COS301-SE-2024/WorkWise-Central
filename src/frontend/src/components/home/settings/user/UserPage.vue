11cd51811413cd7340d37a7c87479c927ab83c4d
Thando Dlamini <u22906160@tuks.co.za>
2024-07-14 17:09:14 +0200

    src: Updates user settings menu

--- src/frontend/src/components/home/settings/user/UserPage.vue
+++ src/frontend/src/components/home/settings/user/UserPage.vue
@@ -7,71 +7,124 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="4" class="pl-15">
+      <v-col cols="12" md="4" class="pl-15">
         <settingsMenu/>
       </v-col>
       <!-- Personal Information -->
-      <v-col cols="5" class="pl-15">
-        <v-label>First Name</v-label>
-        <v-text-field v-model="user.firstName" label="First Name"></v-text-field>
-        <v-label>Surname</v-label>
-        <v-text-field v-model="user.surname" label="Surname"></v-text-field>
-        <v-label>Date of Birth</v-label>
-        <v-text-field v-model="user.dateOfBirth" label="Date of Birth" type="date"></v-text-field>
-        <v-label>Gender</v-label>
-        <v-text-field v-model="user.gender" label="Gender"></v-text-field>
-        <v-label>Preferred Language</v-label>
-        <v-text-field v-model="user.preferredLanguage" label="Preferred Language"></v-text-field>
-        <v-label>Phone</v-label>
-        <v-text-field v-model="user.contactInfo.phone" label="Phone"></v-text-field>
-        <v-label>Email address</v-label>
-        <v-text-field v-model="user.contactInfo.email" label="Email"></v-text-field>
-        <v-label>Street</v-label>
-        <v-text-field v-model="user.address.street" label="Street"></v-text-field>
-        <v-label>Surburb</v-label>
-        <v-text-field v-model="user.address.suburb" label="Suburb"></v-text-field>
-        <v-label>City</v-label>
-        <v-text-field v-model="user.address.city" label="City"></v-text-field>
-        <v-label>Postal Code</v-label>
-        <v-text-field v-model="user.address.postalCode" label="Postal Code"></v-text-field>
-        <v-label>Complex</v-label>
-        <v-text-field v-model="user.address.complex" label="Complex"></v-text-field>
-        <v-label>House Number</v-label>
-        <v-text-field v-model="user.address.houseNumber" label="House Number"></v-text-field>
+      <v-col cols="12" md="5" class="pl-15">
+        <v-form ref="form" @submit.prevent>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.firstName" label="First Name"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.surname" label="Surname"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.dateOfBirth" label="Date of Birth" type="date"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-select v-model="user.gender" :items="genderOptions" label="Gender" variant="solo"></v-select>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-select v-model="user.preferredLanguage" :items="languageOptions" label="Preferred Language" variant="solo"></v-select>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.contactInfo.phone" :rules="phoneRules" label="Phone"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.contactInfo.email" :rules="emailRules" label="Email"></v-text-field>
+            </v-col>
+            <!-- Address fields -->
+            <!-- ... (address fields omitted for brevity) -->
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.address.street" label="Street"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.address.suburb" label="Suburb"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.address.city" label="City"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.address.postalCode" label="Postal Code"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.address.complex" label="Complex"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.address.houseNumber" label="House Number"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
       </v-col>
-      <v-col cols="2" class="pl-15">
-        <v-avatar color="grey" size="150">
-          <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg" cover></v-img>
-        </v-avatar>
+      <v-col cols="12" md="3" class="pl-15">
+        <userAvatar/>
       </v-col>
     </v-row>
   </v-container>
 </template>
-<script setup lang="ts">
-import { ref } from 'vue'
-import Menu from 'primevue/menu'
-import { useRouter } from 'vue-router'
-import settingsMenu from '@/components/home/settings/SettingsMenu.vue'
 
-const user = ref({
-  firstName: '',
-  surname: '',
-  dateOfBirth: '',
-  gender: '',
-  preferredLanguage: '',
-  contactInfo: {
-    phone: '',
-    email: ''
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import settingsMenu from '@/components/home/settings/SettingsMenu.vue';
+import userAvatar from '@/components/home/settings/user/UserProfileAvatar.vue';
+
+export default defineComponent({
+  name: 'UserPage',
+  components: {
+    settingsMenu,
+    userAvatar
   },
-  address: {
-    street: '',
-    suburb: '',
-    city: '',
-    postalCode: '',
-    complex: '',
-    houseNumber: ''
+  data() {
+    return {
+      user: {
+        firstName: '',
+        surname: '',
+        dateOfBirth: '',
+        gender: '',
+        preferredLanguage: '',
+        contactInfo: {
+          phone: '',
+          email: ''
+        },
+        address: {
+          street: '',
+          suburb: '',
+          city: '',
+          postalCode: '',
+          complex: '',
+          houseNumber: ''
+        }
+      },
+      form: {
+        avatar: null
+      },
+      genderOptions: ['Male', 'Female', 'Other'],
+      languageOptions: [
+        'Afrikaans', 'English', 'isiNdebele', 'isiXhosa', 'isiZulu',
+        'Sepedi', 'Sesotho', 'Setswana', 'siSwati', 'Tshivenda', 'Xitsonga'
+      ],
+      phoneRules: [
+        v => !!v && /^[0-9]{10}$/.test(v) || 'Phone number must be 10 digits long and numeric'
+      ],
+      emailRules: [
+        v => !!v && /.+@.+/.test(v) || 'E-mail must be valid'
+      ]
+    };
+  },
+  setup() {
+    const router = useRouter();
+    return { router };
   }
-})
-
+});
 </script>
-<style></style>
+
+<style scoped>
+.pl-15 {
+  padding-left: 15px;
+}
+</style>
+
+
