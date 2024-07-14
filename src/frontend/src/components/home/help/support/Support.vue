0e0875e78b6b34f083be1067e150e78303b639e4
bob dlm <118727166+CodeHermez@users.noreply.github.com>
2024-07-14 14:14:23 +0200

    feature: addition of stepper to the tutorial page to be able to show different tutorial of how to use the system with certain steps

--- src/frontend/src/components/home/help/support/Support.vue
+++ src/frontend/src/components/home/help/support/Support.vue
@@ -22,7 +22,8 @@
                       :key="i"
                       :text="faqs[i].answer"
                       :title="faqs[i].question"
-                      :color="isdarkmode === true ? '#22272B' : '#EEEEEE'"
+                      :theme="isdarkmode === true ? 'dark' : 'light'"
+                      style="font-family: Nunito, sans-serif"
                     ></v-expansion-panel>
                   </v-expansion-panels>
                 </div>
@@ -30,31 +31,90 @@
             </v-col>
           </v-tabs-window-item>
           <v-tabs-window-item :value="2">
-            <v-col :cols="12"> </v-col>
-          </v-tabs-window-item>
-          <v-tabs-window-item :value="3">
             <v-col :cols="12"
-              ><v-card>
-                <v-card-title>
-                  <v-avatar size="40" class="mr-4">
-                    <img
-                      src="https://github.com/COS301-SE-2024/WorkWise-Central/blob/develop/res/img/team%20photos/Bob.jpg"
-                      alt="Avatar"
-                    />
-                  </v-avatar>
-                  Tinashe Fadza
-                </v-card-title>
-                <v-card-subtitle>Software Engineer</v-card-subtitle>
-                <v-card-text>
-                  <div><strong>Email:</strong> ttfadzai@example.com</div>
-                  <div><strong>Phone:</strong> +27 73 441 4451</div>
-                  <div><strong>Address:</strong> 1234 Lunnon Street, Hatcrest, PTA</div>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn color="primary" @click="sendEmail">Email</v-btn>
-                  <v-btn color="secondary" @click="callPhone">Call</v-btn>
-                </v-card-actions>
-              </v-card></v-col
+              ><v-stepper v-model="step" :items="items" show-actions>
+                <template v-slot:item.1>
+                  <h3 class="text-h6">Order</h3>
+
+                  <br />
+
+                  <v-sheet border>
+                    <v-table>
+                      <thead>
+                        <tr>
+                          <th>Description</th>
+                          <th class="text-end">Quantity</th>
+                          <th class="text-end">Price</th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        <tr v-for="(product, index) in products" :key="index">
+                          <td v-text="product.name"></td>
+                          <td class="text-end" v-text="product.quantity"></td>
+                          <td class="text-end" v-text="product.quantity * product.price"></td>
+                        </tr>
+
+                        <tr>
+                          <th>Total</th>
+                          <th></th>
+                          <th class="text-end">${{ subtotal }}</th>
+                        </tr>
+                      </tbody>
+                    </v-table>
+                  </v-sheet>
+                </template>
+
+                <template v-slot:item.2>
+                  <h3 class="text-h6">Shipping</h3>
+
+                  <br />
+
+                  <v-radio-group v-model="shipping" label="Delivery Method">
+                    <v-radio label="Standard Shipping" value="5"></v-radio>
+                    <v-radio label="Priority Shipping" value="10"></v-radio>
+                    <v-radio label="Express Shipping" value="15"></v-radio>
+                  </v-radio-group>
+                </template>
+
+                <template v-slot:item.3>
+                  <h3 class="text-h6">Confirm</h3>
+
+                  <br />
+
+                  <v-sheet border>
+                    <v-table>
+                      <thead>
+                        <tr>
+                          <th>Description</th>
+                          <th class="text-end">Quantity</th>
+                          <th class="text-end">Price</th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        <tr v-for="(product, index) in products" :key="index">
+                          <td v-text="product.name"></td>
+                          <td class="text-end" v-text="product.quantity"></td>
+                          <td class="text-end" v-text="product.quantity * product.price"></td>
+                        </tr>
+
+                        <tr>
+                          <td>Shipping</td>
+                          <td></td>
+                          <td class="text-end" v-text="shipping"></td>
+                        </tr>
+
+                        <tr>
+                          <th>Total</th>
+                          <th></th>
+                          <th class="text-end">${{ total }}</th>
+                        </tr>
+                      </tbody>
+                    </v-table>
+                  </v-sheet>
+                </template>
+              </v-stepper></v-col
             >
           </v-tabs-window-item>
         </v-tabs-window>
@@ -148,7 +208,30 @@ export default defineComponent({
           answer:
             "Yes, you can view all jobs assigned to you by clicking on the 'My Tasks' section from the dashboard. This view consolidates jobs from all projects, making it easier for you to manage your workload."
         }
+      ],
+      shipping: 0,
+      step: 1,
+      items: ['Review Order', 'Select Shipping', 'Submit'],
+      products: [
+        {
+          name: 'Product 1',
+          price: 10,
+          quantity: 2
+        },
+        {
+          name: 'Product 2',
+          price: 15,
+          quantity: 10
+        }
       ]
+    }
+  },
+  computed: {
+    subtotal() {
+      return this.products.reduce((acc, product) => acc + product.quantity * product.price, 0)
+    },
+    total() {
+      return this.subtotal + Number(this.shipping ?? 0)
     }
   },
   methods: {
