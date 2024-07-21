91ba5a91dc163a8c2cd8762db78abc1ba0d8538f
Hamza Mokiwa <u22575172@tuks.co.za>
2024-07-21 20:33:09 +0200

    feat: Adds Populated and business-specific queries for Admins

--- src/api-gateway/src/admin/admin.repository.ts
+++ src/api-gateway/src/admin/admin.repository.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserJoinRequest } from './entities/request-to-join.entity';
-import { Model, Types } from 'mongoose';
-import { isNotDeleted } from '../shared/soft-delete';
+import { FlattenMaps, Model, Types } from 'mongoose';
 
 @Injectable()
 export class AdminRepository {
@@ -19,11 +18,27 @@ export class AdminRepository {
     return result;
   }
 
+  async acceptRequest(userId: Types.ObjectId, companyId: Types.ObjectId) {
+    const result = await this.userJoinRequestModel.deleteOne({
+      $and: [{ userToJoin: userId }, { companyId: companyId }],
+    });
+    console.log('Accept', result);
+    return result;
+  }
+
+  async rejectRequest(userId: Types.ObjectId, companyId: Types.ObjectId) {
+    const result = await this.userJoinRequestModel.deleteOne({
+      $and: [{ userToJoin: userId }, { companyId: companyId }],
+    });
+    console.log('Reject', result);
+    return result;
+  }
+
   async cancelRequest(userId: Types.ObjectId, companyId: Types.ObjectId) {
     const result = await this.userJoinRequestModel.deleteOne({
-      $and: [{ userToJoin: userId }, { companyId: companyId }, isNotDeleted],
+      $and: [{ userToJoin: userId }, { companyId: companyId }],
     });
-    console.log(result);
+    console.log('cancelRequest', result);
     return result;
   }
 
@@ -31,7 +46,7 @@ export class AdminRepository {
     console.log('findOneJoinRequest');
     const result = await this.userJoinRequestModel
       .findOne({
-        $and: [{ _id: id }, { isNotDeleted }],
+        _id: id,
       })
       .lean();
     console.log(result);
@@ -40,32 +55,58 @@ export class AdminRepository {
 
   async findAllRequests() {
     console.log('FindAll');
-    const result = await this.userJoinRequestModel
-      .findOne({
-        isNotDeleted,
-      })
-      .lean();
-    console.log(result);
+    const result = await this.userJoinRequestModel.find().lean();
+    console.log('result', result);
     return result;
   }
 
-  async findAllRequestsFromUser(userId: Types.ObjectId) {
+  async findRequestsFromUserForCompany(
+    userId: Types.ObjectId,
+    companyId: Types.ObjectId,
+  ) {
+    return this.userJoinRequestModel
+      .find({
+        $and: [{ userToJoin: userId }, { companyId: companyId }],
+      })
+      .lean();
+  }
+
+  async findAllRequestsFromUser(
+    userId: Types.ObjectId,
+  ): Promise<(FlattenMaps<UserJoinRequest> & { _id: Types.ObjectId })[]> {
     console.log('findAllRequestsFromUser');
     const result = await this.userJoinRequestModel
-      .findOne({
-        $and: [{ userToJoin: userId }, { isNotDeleted }],
+      .find({
+        $and: [{ userToJoin: userId }],
       })
       .lean();
     console.log(result);
     return result;
   }
 
-  async findAllRequestsForCompany(companyId: Types.ObjectId) {
+  async findAllRequestsForCompany(
+    companyId: Types.ObjectId,
+  ): Promise<(FlattenMaps<UserJoinRequest> & { _id: Types.ObjectId })[]> {
     console.log('findAllRequestsForCompany');
     const result = await this.userJoinRequestModel
-      .findOne({
-        $and: [{ companyId: companyId }, { isNotDeleted }],
+      .find({
+        companyId: companyId,
       })
+      .lean();
+    console.log(result);
+    return result;
+  }
+
+  async findAllDetailedRequestsForCompany(
+    companyId: Types.ObjectId,
+  ): Promise<(FlattenMaps<UserJoinRequest> & { _id: Types.ObjectId })[]> {
+    console.log('findAllRequestsForCompanyDetailed');
+    const result = await this.userJoinRequestModel
+      .find({
+        companyId: companyId,
+      })
+      .populate(['userToJoin', 'roleId'])
+      .sort({ createdAt: -1 })
       .lean();
     console.log(result);
     return result;
