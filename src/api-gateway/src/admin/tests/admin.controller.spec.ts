421c74a7f2894a2ef4cb471d13b5a81129ad8a0a
Hamza Mokiwa <u22575172@tuks.co.za>
2024-07-21 21:01:07 +0200

    fix: Resolved test errors

--- src/api-gateway/src/admin/tests/admin.controller.spec.ts
+++ src/api-gateway/src/admin/tests/admin.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminController } from '../admin.controller';
 import { AdminService } from '../admin.service';
+import { MockFunctionMetadata, ModuleMocker } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('InviteController', () => {
   let controller: AdminController;
@@ -9,7 +12,17 @@ describe('InviteController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AdminController],
       providers: [AdminService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<AdminController>(AdminController);
   });
--- src/api-gateway/src/admin/tests/admin.service.spec.ts
+++ src/api-gateway/src/admin/tests/admin.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from '../admin.service';
+import { MockFunctionMetadata, ModuleMocker } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('InviteService', () => {
   let service: AdminService;
@@ -7,7 +10,17 @@ describe('InviteService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AdminService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     service = module.get<AdminService>(AdminService);
   });
