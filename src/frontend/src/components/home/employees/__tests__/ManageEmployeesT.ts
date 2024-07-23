1e726d4058760493cb9b284503efd8855ab902d7
bob dlm <118727166+CodeHermez@users.noreply.github.com>
2024-07-23 12:07:12 +0200

    refactor: changed the name of ManageEmployeesT.vue to ManageEmployees.vue for both the view and the table component

--- src/frontend/src/components/home/employees/__tests__/ManageEmployeesT.ts
+++ src/frontend/src/components/home/employees/__tests__/ManageEmployeesT.ts
@@ -1,6 +1,6 @@
 import { createVuetify } from 'vuetify'
 import { mount } from 'cypress/vue'
-import ManageEmployeesT from '../management/ManageEmployeesT.vue'
+import ManageEmployeesT from '../management/ManageEmployees.vue'
 
 it('renders manage employees', () => {
   const vuetify = createVuetify()
