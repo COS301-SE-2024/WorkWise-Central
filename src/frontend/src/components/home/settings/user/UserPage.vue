f619fec27b02c0ef5c54ab8d0a01ccd874252643
Thando Dlamini <u22906160@tuks.co.za>
2024-07-24 12:31:51 +0200

    src: Updates user settings page

--- src/frontend/src/components/home/settings/user/UserPage.vue
+++ src/frontend/src/components/home/settings/user/UserPage.vue
@@ -8,87 +8,179 @@
     </v-row>
     <v-container class="d-flex flex-column align-center justify-center">
       <v-row>
-        <v-col cols="12" md="3" class="text-center" sm="12">
+        <v-col cols="12" md="3" class="text-center" sm="12" order-sm="1" order-md="0" order="1">
           <userAvatar />
         </v-col>
         <!-- Personal Information -->
-        <v-col cols="12" md="5">
+        <v-col cols="12" md="5" order-sm="2" order-md="1" order="2">
           <v-form ref="form" @submit.prevent>
             <v-row>
-              <v-col cols="12" sm="6">
-                <v-text-field v-model="user.firstName" label="First Name"></v-text-field>
+              <!-- Personal Information Header -->
+              <v-col cols="12">
+                <v-divider>
+                  <h5 class="mb-2">Personal Information</h5>
+                </v-divider>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field v-model="user.surname" label="Surname"></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="firstName" class="v-text-field-label">First Name</label>
+                  <v-text-field
+                      id="firstName"
+                      v-model="user.firstName"
+                      hide-details
+                  ></v-text-field>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field
-                  v-model="user.dateOfBirth"
-                  label="Date of Birth"
-                  type="date"
-                ></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="surname" class="v-text-field-label">Surname</label>
+                  <v-text-field
+                      id="surname"
+                      v-model="user.surname"
+                      hide-details
+                  ></v-text-field>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-select
-                  v-model="user.gender"
-                  :items="genderOptions"
-                  label="Gender"
-                  variant="solo"
-                ></v-select>
+                <div class="v-text-field-wrapper">
+                  <label for="dateOfBirth" class="v-text-field-label">Date of Birth</label>
+                  <v-text-field
+                      id="dateOfBirth"
+                      v-model="user.dateOfBirth"
+                      type="date"
+                  ></v-text-field>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-select
-                  v-model="user.preferredLanguage"
-                  :items="languageOptions"
-                  label="Preferred Language"
-                  variant="solo"
-                ></v-select>
+                <div class="v-text-field-wrapper">
+                  <label for="gender" class="v-text-field-label">Gender</label>
+                  <v-select
+                      id="gender"
+                      v-model="user.gender"
+                      :items="genderOptions"
+                      variant="solo"
+                  ></v-select>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field
-                  v-model="user.contactInfo.phone"
-                  :rules="phoneRules"
-                  label="Phone"
-                ></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="preferredLanguage" class="v-text-field-label">Preferred Language</label>
+                  <v-select
+                      id="preferredLanguage"
+                      v-model="user.preferredLanguage"
+                      :items="languageOptions"
+                      variant="solo"
+                  ></v-select>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field
-                  v-model="user.contactInfo.email"
-                  :rules="emailRules"
-                  label="Email"
-                ></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="phone" class="v-text-field-label">Phone</label>
+                  <v-text-field
+                      id="phone"
+                      v-model="user.contactInfo.phone"
+                      :rules="phoneRules"
+                  ></v-text-field>
+                </div>
               </v-col>
-              <!-- Address fields -->
-              <!-- ... (address fields omitted for brevity) -->
+
               <v-col cols="12" sm="6">
-                <v-text-field v-model="user.address.street" label="Street"></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="email" class="v-text-field-label">Email</label>
+                  <v-text-field
+                      id="email"
+                      v-model="user.contactInfo.email"
+                      :rules="emailRules"
+                  ></v-text-field>
+                </div>
               </v-col>
-              <v-col cols="12" sm="6">
-                <v-text-field v-model="user.address.suburb" label="Suburb"></v-text-field>
+
+              <!-- Address Information Header -->
+              <v-col cols="12">
+                <v-divider>
+                  <h5 class="mb-2">Address Information</h5>
+                </v-divider>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field v-model="user.address.city" label="City"></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="street" class="v-text-field-label">Street</label>
+                  <v-text-field
+                      id="street"
+                      v-model="user.address.street"
+                      hide-details
+                  ></v-text-field>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field v-model="user.address.postalCode" label="Postal Code"></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="suburb" class="v-text-field-label">Suburb</label>
+                  <v-text-field
+                      id="suburb"
+                      v-model="user.address.suburb"
+                      hide-details
+                  ></v-text-field>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field v-model="user.address.complex" label="Complex"></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="city" class="v-text-field-label">City</label>
+                  <v-text-field
+                      id="city"
+                      v-model="user.address.city"
+                      hide-details
+                  ></v-text-field>
+                </div>
               </v-col>
+
               <v-col cols="12" sm="6">
-                <v-text-field
-                  v-model="user.address.houseNumber"
-                  label="House Number"
-                ></v-text-field>
+                <div class="v-text-field-wrapper">
+                  <label for="postalCode" class="v-text-field-label">Postal Code</label>
+                  <v-text-field
+                      id="postalCode"
+                      v-model="user.address.postalCode"
+                      hide-details
+                  ></v-text-field>
+                </div>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <div class="v-text-field-wrapper">
+                  <label for="complex" class="v-text-field-label">Complex</label>
+                  <v-text-field
+                      id="complex"
+                      v-model="user.address.complex"
+                      hide-details
+                  ></v-text-field>
+                </div>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <div class="v-text-field-wrapper">
+                  <label for="houseNumber" class="v-text-field-label">House Number</label>
+                  <v-text-field
+                      id="houseNumber"
+                      v-model="user.address.houseNumber"
+                  ></v-text-field>
+                </div>
               </v-col>
             </v-row>
+
             <div class="text-center">
               <v-btn color="primary" @click="submitForm">Update Profile</v-btn>
             </div>
           </v-form>
         </v-col>
         <!-- This should not be visible for       -->
-        <v-col cols="12" md="4">
+        <v-col cols="12" md="4" order-sm="0" order-md="2" order="0" class="pt-8">
           <settingsMenu />
         </v-col>
       </v-row>
