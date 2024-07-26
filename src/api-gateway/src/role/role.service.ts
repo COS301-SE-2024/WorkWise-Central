69bd283dcc4f84cb71266a379ca19ede198837be
JessicaBloem <jessicabloem8@gmail.com>
2024-07-26 13:11:07 +0200

    feature/role-based-access: roles and permissions revised

--- src/api-gateway/src/role/role.service.ts
+++ src/api-gateway/src/role/role.service.ts
@@ -30,23 +30,34 @@ export class RoleService {
     this.permissionsArray.push('edit all employees');
     this.permissionsArray.push('edit employees under me');
     this.permissionsArray.push('add new employees');
+    this.permissionsArray.push('remove any employees');
+    this.permissionsArray.push('remove employees under me');
     this.permissionsArray.push('view all jobs');
-    this.permissionsArray.push('view all jobs under me');
-    this.permissionsArray.push('view all jobs assigned to me');
+    this.permissionsArray.push('view jobs under me');
+    this.permissionsArray.push('view jobs assigned to me');
     this.permissionsArray.push('edit all jobs');
     this.permissionsArray.push('edit jobs that are under me');
     this.permissionsArray.push('edit jobs that are assigned to me');
     this.permissionsArray.push('add a new job');
+    this.permissionsArray.push('remove any job');
+    this.permissionsArray.push('remove job under me');
+    this.permissionsArray.push('remove job assigned to me');
     this.permissionsArray.push('view all clients');
-    this.permissionsArray.push('view all clients under me');
-    this.permissionsArray.push('view all clients that are assigned to me');
+    this.permissionsArray.push('view clients under me');
+    this.permissionsArray.push('view clients that are assigned to me');
     this.permissionsArray.push('edit all clients');
-    this.permissionsArray.push('edit all clients that are under me');
-    this.permissionsArray.push('edit all clients that are assigned to me');
+    this.permissionsArray.push('edit clients that are under me');
+    this.permissionsArray.push('edit clients that are assigned to me');
+    this.permissionsArray.push('add a new clients');
+    this.permissionsArray.push('remove any clients');
+    this.permissionsArray.push('remove clients under me');
+    this.permissionsArray.push('remove clients assigned to me');
     this.permissionsArray.push('view all inventory');
     this.permissionsArray.push('edit all inventory');
-    this.permissionsArray.push('add a new inventory item');
+    this.permissionsArray.push('add new inventory item');
     this.permissionsArray.push('record inventory use');
+    this.permissionsArray.push('record job details');
+    this.permissionsArray.push('company settings');
   }
 
   async validateCreateRole(role: CreateRoleDto) {
@@ -89,6 +100,12 @@ export class RoleService {
         return new ValidationResult(false, `Role already exists`);
       }
     } catch (error) {}
+
+    //Checking that the role is not Default or owner
+    if (roleObj.roleName === 'Owner' || roleObj.roleName === 'Default') {
+      return new ValidationResult(false, `Not allowed to edit this role`);
+    }
+
     return new ValidationResult(true, `All good`);
   }
 
@@ -119,7 +136,14 @@ export class RoleService {
     if (!(await this.companyService.companyIdExists(companyId))) {
       throw new Error('CompanyId does not exist');
     }
-    const result = await this.roleRepository.findAllInCompany(companyId);
+    const roles = await this.roleRepository.findAllInCompany(companyId);
+    let result;
+    //Remove owner role and default role
+    roles.forEach((role) => {
+      if (role.roleName !== 'Owner' && role.roleName !== 'Default') {
+        result.push(role);
+      }
+    });
     return result;
   }
 
@@ -128,13 +152,22 @@ export class RoleService {
     if (!(await this.companyService.companyIdExists(companyId))) {
       throw new Error('CompanyId does not exist');
     }
-    const result = await this.roleRepository.findByIdInCompany(name, companyId);
-    return result;
+    if (name !== 'Owner' && name !== 'Default') {
+      const result = await this.roleRepository.findByIdInCompany(
+        name,
+        companyId,
+      );
+      return result;
+    }
+    return null;
   }
 
   async findById(identifier: Types.ObjectId) {
     const result = await this.roleRepository.findById(identifier);
-    return result;
+    if (result.roleName !== 'Owner' && result.roleName !== 'Default') {
+      return result;
+    }
+    return null;
   }
 
   async update(id: Types.ObjectId, updateRoleDto: UpdateRoleDto) {
@@ -150,6 +183,18 @@ export class RoleService {
     return await this.roleRepository.roleExists(id);
   }
 
+  async hasPermission(permission: string, id: Types.ObjectId) {
+    //checking if the role exists
+    if (!(await this.roleExists(id))) {
+      throw new Error('role not found');
+    }
+    const role = await this.findById(id);
+    if (role.permissionSuite.includes(permission)) {
+      return true;
+    }
+    return false;
+  }
+
   async roleExistsInCompany(
     id: Types.ObjectId,
     companyId: Types.ObjectId,
@@ -163,8 +208,13 @@ export class RoleService {
 
   async remove(id: Types.ObjectId): Promise<boolean> {
     //Checking if the role exists
-    if (!(await this.roleExists(id))) {
+    const role = await this.findById(id);
+    if (!role) {
       throw new NotFoundException('Role not found');
+    }
+    //Checking that the role is not Owner or Default
+    if (role.roleName === 'Owner' || role.roleName === 'Default') {
+      throw new Error('Not allowed to remove this role');
     }
     return this.roleRepository.remove(id);
   }
@@ -183,28 +233,18 @@ export class RoleService {
     adminRoleDto.companyId = companyId;
     adminRoleDto.roleName = 'Admin';
     adminRoleDto.permissionSuite.push('view all employees');
-    adminRoleDto.permissionSuite.push('view employees under me');
     adminRoleDto.permissionSuite.push('edit all employees');
-    adminRoleDto.permissionSuite.push('edit employees under me');
+    adminRoleDto.permissionSuite.push('add new employees');
+    adminRoleDto.permissionSuite.push('remove any employees');
     adminRoleDto.permissionSuite.push('view all jobs');
-    adminRoleDto.permissionSuite.push('view all jobs under me');
-    adminRoleDto.permissionSuite.push('view all jobs assigned to me');
     adminRoleDto.permissionSuite.push('edit all jobs');
-    adminRoleDto.permissionSuite.push('edit jobs that are under me');
-    adminRoleDto.permissionSuite.push('edit jobs that are assigned to me');
     adminRoleDto.permissionSuite.push('add a new job');
+    adminRoleDto.permissionSuite.push('remove any job');
     adminRoleDto.permissionSuite.push('view all clients');
-    adminRoleDto.permissionSuite.push('view all clients under me');
-    adminRoleDto.permissionSuite.push(
-      'view all clients that are assigned to me',
-    );
     adminRoleDto.permissionSuite.push('edit all clients');
-    adminRoleDto.permissionSuite.push('edit all clients that are under me');
-    adminRoleDto.permissionSuite.push(
-      'edit all clients that are assigned to me',
-    );
-    adminRoleDto.permissionSuite.push('view all inventory');
-    adminRoleDto.permissionSuite.push('record inventory use');
+    adminRoleDto.permissionSuite.push('add a new clients');
+    adminRoleDto.permissionSuite.push('remove any clients');
+    adminRoleDto.permissionSuite.push('company settings');
 
     let newRole = await this.create(adminRoleDto);
     await this.roleRepository.save(newRole);
@@ -213,23 +253,22 @@ export class RoleService {
     const foremanRoleDto = new CreateRoleDto();
     foremanRoleDto.companyId = companyId;
     foremanRoleDto.roleName = 'Foreman';
-    adminRoleDto.permissionSuite.push('view all employees under me');
-    foremanRoleDto.permissionSuite.push('view all jobs under me');
-    foremanRoleDto.permissionSuite.push('view all jobs assigned to me');
-    foremanRoleDto.permissionSuite.push('edit all jobs');
+    foremanRoleDto.permissionSuite.push('view employees under me');
+    foremanRoleDto.permissionSuite.push('edit employees under me');
+    foremanRoleDto.permissionSuite.push('remove employees under me');
+    foremanRoleDto.permissionSuite.push('view jobs under me');
+    foremanRoleDto.permissionSuite.push('view jobs assigned to me');
     foremanRoleDto.permissionSuite.push('edit jobs that are under me');
     foremanRoleDto.permissionSuite.push('edit jobs that are assigned to me');
-    foremanRoleDto.permissionSuite.push('add a new job');
-    foremanRoleDto.permissionSuite.push('view all clients under me');
-    foremanRoleDto.permissionSuite.push(
-      'view all clients that are assigned to me',
-    );
-    foremanRoleDto.permissionSuite.push('edit all clients that are under me');
-    foremanRoleDto.permissionSuite.push(
-      'edit all clients that are assigned to me',
-    );
-    foremanRoleDto.permissionSuite.push('view all inventory');
-    foremanRoleDto.permissionSuite.push('record inventory use');
+    foremanRoleDto.permissionSuite.push('remove job under me');
+    foremanRoleDto.permissionSuite.push('remove job assigned to me');
+    foremanRoleDto.permissionSuite.push('view clients under me');
+    foremanRoleDto.permissionSuite.push('view clients that are assigned to me');
+    foremanRoleDto.permissionSuite.push('edit clients that are under me');
+    foremanRoleDto.permissionSuite.push('edit clients that are assigned to me');
+    foremanRoleDto.permissionSuite.push('remove clients under me');
+    foremanRoleDto.permissionSuite.push('remove clients assigned to me');
+    foremanRoleDto.permissionSuite.push('record job details');
 
     newRole = await this.create(foremanRoleDto);
     await this.roleRepository.save(newRole);
@@ -238,20 +277,12 @@ export class RoleService {
     const teamRoleDto = new CreateRoleDto();
     teamRoleDto.companyId = companyId;
     teamRoleDto.roleName = 'Team leader';
-    teamRoleDto.permissionSuite.push('edit all employees');
-    teamRoleDto.permissionSuite.push('view all jobs assigned to me');
-    teamRoleDto.permissionSuite.push('edit all jobs');
-    teamRoleDto.permissionSuite.push('edit jobs that are assigned to me');
-    teamRoleDto.permissionSuite.push('view all clients');
-    teamRoleDto.permissionSuite.push(
-      'view all clients that are assigned to me',
-    );
-    teamRoleDto.permissionSuite.push('edit all clients');
-    teamRoleDto.permissionSuite.push(
-      'edit all clients that are assigned to me',
-    );
-    teamRoleDto.permissionSuite.push('view all inventory');
-    teamRoleDto.permissionSuite.push('record inventory use');
+    teamRoleDto.permissionSuite.push('view employees under me');
+    teamRoleDto.permissionSuite.push('view jobs under me');
+    teamRoleDto.permissionSuite.push('view jobs assigned to me');
+    teamRoleDto.permissionSuite.push('view clients under me');
+    teamRoleDto.permissionSuite.push('view clients that are assigned to me');
+    teamRoleDto.permissionSuite.push('record job details');
 
     newRole = await this.create(teamRoleDto);
     await this.roleRepository.save(newRole);
@@ -262,7 +293,7 @@ export class RoleService {
     inventoryRoleDto.roleName = 'Inventory manager';
     inventoryRoleDto.permissionSuite.push('view all inventory');
     inventoryRoleDto.permissionSuite.push('edit all inventory');
-    inventoryRoleDto.permissionSuite.push('add a new inventory item');
+    inventoryRoleDto.permissionSuite.push('add new inventory item');
     inventoryRoleDto.permissionSuite.push('record inventory use');
 
     newRole = await this.create(inventoryRoleDto);
@@ -272,20 +303,22 @@ export class RoleService {
     const workerRoleDto = new CreateRoleDto();
     workerRoleDto.companyId = companyId;
     workerRoleDto.roleName = 'Worker';
-    workerRoleDto.permissionSuite.push('view all jobs assigned to me');
-    workerRoleDto.permissionSuite.push('edit jobs that are assigned to me');
-    workerRoleDto.permissionSuite.push(
-      'view all clients that are assigned to me',
-    );
-    workerRoleDto.permissionSuite.push('edit all clients');
-    workerRoleDto.permissionSuite.push('edit all clients that are under me');
-    workerRoleDto.permissionSuite.push(
-      'edit all clients that are assigned to me',
-    );
-    workerRoleDto.permissionSuite.push('view all inventory');
-    workerRoleDto.permissionSuite.push('record inventory use');
+    workerRoleDto.permissionSuite.push('view jobs assigned to me');
+    workerRoleDto.permissionSuite.push('view clients that are assigned to me');
+    workerRoleDto.permissionSuite.push('record job details');
 
     newRole = await this.create(workerRoleDto);
+    await this.roleRepository.save(newRole);
+
+    // Default role
+    const defaultRoleDto = new CreateRoleDto();
+    defaultRoleDto.companyId = companyId;
+    defaultRoleDto.roleName = 'Default';
+    defaultRoleDto.permissionSuite.push('view jobs assigned to me');
+    defaultRoleDto.permissionSuite.push('view clients that are assigned to me');
+    defaultRoleDto.permissionSuite.push('record job details');
+
+    newRole = await this.create(defaultRoleDto);
     await this.roleRepository.save(newRole);
   }
 }
