3145e9f1e6dc95b0fc35cd6360154b94920ab3ac
Hamza Mokiwa <u22575172@tuks.co.za>
2024-07-28 18:07:28 +0200

    fix: firebase-messaging-sw.js build error

--- src/frontend/public/firebase-messaging-sw.js
+++ src/frontend/public/firebase-messaging-sw.js
@@ -1,9 +1,5 @@
-import { getMessaging } from 'firebase/messaging'
+//import { getMessaging } from 'firebase/messaging'
 import { initializeApp } from 'firebase/app'
-
-importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js')
 
 const firebaseConfig = {
   apiKey: '',
@@ -16,4 +12,4 @@ const firebaseConfig = {
 }
 
 export const app = initializeApp(firebaseConfig)
-const messaging = getMessaging(app)
+//const messaging = getMessaging(app)
