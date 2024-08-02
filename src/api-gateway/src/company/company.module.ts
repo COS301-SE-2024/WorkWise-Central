52f660a13a5ae151ca55e5feea280fd0c58dd031
Hamza Mokiwa <u22575172@tuks.co.za>
2024-08-02 03:39:35 +0200

    fix: Removed unnecessary imports

--- src/api-gateway/src/company/company.module.ts
+++ src/api-gateway/src/company/company.module.ts
@@ -6,10 +6,7 @@ import { Company, CompanySchema } from './entities/company.entity';
 import { UsersModule } from '../users/users.module';
 import { CompanyRepository } from './company.repository';
 import { RoleService } from '../role/role.service';
-import { Role, RoleSchema } from '../role/entity/role.entity';
 import { EmployeeService } from '../employee/employee.service';
-import { Employee, EmployeeSchema } from '../employee/entities/employee.entity';
-import { User, UserSchema } from '../users/entities/user.entity';
 import { JobService } from '../job/job.service';
 import { JobModule } from '../job/job.module';
 import { TeamModule } from '../team/team.module';
@@ -18,31 +15,14 @@ import { ClientModule } from '../client/client.module';
 import { RoleModule } from '../role/role.module';
 import { UsersService } from '../users/users.service';
 import { EmailModule } from '../email/email.module';
-import { RoleRepository } from 'src/role/role.repository';
-import { EmployeeRepository } from 'src/employee/employee.repository';
-import { TeamRepository } from 'src/team/team.repository';
 import { JwtService } from '@nestjs/jwt';
 import { FileModule } from '../file/file.module';
 import { FileService } from '../file/file.service';
-import {
-  JobPriorityTag,
-  JobPriorityTagSchema,
-  JobTag,
-  JobTagSchema,
-} from '../job/entities/job-tag.entity';
-import { JobStatus, JobStatusSchema } from '../job/entities/job-status.entity';
+import { EmployeeModule } from '../employee/employee.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Company.name, schema: CompanySchema },
-      { name: Role.name, schema: RoleSchema },
-      { name: User.name, schema: UserSchema },
-      { name: Employee.name, schema: EmployeeSchema },
-      { name: JobTag.name, schema: JobTagSchema },
-      { name: JobPriorityTag.name, schema: JobPriorityTagSchema },
-      { name: JobStatus.name, schema: JobStatusSchema },
-    ]),
+    MongooseModule.forFeature([{ name: Company.name, schema: CompanySchema }]),
     forwardRef(() => UsersModule),
     forwardRef(() => JobModule),
     forwardRef(() => TeamModule),
@@ -50,18 +30,16 @@ import { JobStatus, JobStatusSchema } from '../job/entities/job-status.entity';
     forwardRef(() => RoleModule),
     forwardRef(() => EmailModule),
     forwardRef(() => FileModule),
+    forwardRef(() => EmployeeModule),
   ],
   controllers: [CompanyController],
   providers: [
     CompanyService,
     CompanyRepository,
     RoleService,
-    RoleRepository,
     EmployeeService,
-    EmployeeRepository,
     JobService,
     TeamService,
-    TeamRepository,
     UsersService,
     JwtService,
     FileService,
