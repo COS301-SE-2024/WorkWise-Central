1c2729ab1a72f70e1f034f87bc7485f472e009e2
Hamza <131655930+CRYatUP@users.noreply.github.com>
2024-08-26 11:43:25 +0200

    feat: Adds basic ChatGateway for real-time chat

---
+++ src/api-gateway/src/chat/chat.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+import { AddMessageDto } from './dto/add-message.dto';
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private logger = new Logger('ChatGateway');
+
+  @SubscribeMessage('chat')
+  handleMessage(@MessageBody() payload: AddMessageDto): AddMessageDto {
+    this.logger.log(`Message received: ${payload.author} - ${payload.body}`);
+    this.server.emit('chat', payload);
+    return payload;
+  }
+
+  handleConnection(socket: Socket) {
+    this.logger.log(`Socket connected: ${socket.id}`);
+  }
+
+  handleDisconnect(socket: Socket) {
+    this.logger.log(`Socket disconnected: ${socket.id}`);
+  }
+}
