9316aa41b55a0a9319535637594b51392fd97f23
Hamza Mokiwa <u22575172@tuks.co.za>
2024-08-28 21:16:33 +0200

    feat: Adds validated time-tracking Dtos and Entity

---
+++ src/api-gateway/src/time-tracker/entities/time-tracker.entity.ts
@@ -0,0 +1,53 @@
+import { SchemaTypes, Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Employee } from '../../employee/entities/employee.entity';
+import { Job } from '../../job/entities/job.entity';
+import { currentDate } from '../../utils/Utils';
+import { Company } from '../../company/entities/company.entity';
+
+export class TimeInterval {
+  constructor(start: Date) {
+    this.start = start;
+  }
+
+  @ApiProperty()
+  @Prop({ type: Date, required: true, default: null })
+  start: Date;
+  @ApiProperty()
+  @Prop({ type: Date, required: false })
+  end?: Date;
+}
+
+@Schema()
+export class TimeTracker {
+  @ApiProperty()
+  @Prop({ type: SchemaTypes.ObjectId, required: true, ref: Employee.name, index: true })
+  employeeId: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ type: SchemaTypes.ObjectId, required: true, ref: Job.name, index: true })
+  jobId: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ type: SchemaTypes.ObjectId, required: true, ref: Company.name, index: true })
+  companyId: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ type: Date, required: true, default: currentDate() })
+  checkInTime: Date;
+
+  @ApiProperty()
+  @Prop({ type: Date, required: true, default: null })
+  checkOutTime?: Date = null;
+
+  @ApiProperty()
+  @Prop({ type: TimeInterval, required: true, default: [] })
+  pauses: TimeInterval[] = [];
+
+  @ApiProperty()
+  @Prop({ type: Date, required: true, default: currentDate() })
+  createdAt: Date = currentDate();
+}
+
+export const TimeTrackerSchema = SchemaFactory.createForClass(TimeTracker);
