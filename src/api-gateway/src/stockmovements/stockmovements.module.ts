4624daaeb2d931bce085fede41fae0e692c66204
JessicaBloem <jessicabloem8@gmail.com>
2024-09-02 06:26:31 +0200

    feature: added stock movements

---
+++ src/api-gateway/src/stockmovements/stockmovements.module.ts
@@ -0,0 +1,37 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { StockMovementsService } from './stockmovements.service';
+import { StockMovementsController } from './stockmovements.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StockMovements, StockMovementsSchema } from './entities/stockmovements.entity';
+import { UsersModule } from '../users/users.module';
+import { CompanyModule } from '../company/company.module';
+import { RoleModule } from '../role/role.module';
+import { JobModule } from '../job/job.module';
+import { TeamModule } from '../team/team.module';
+import { JobService } from '../job/job.service';
+import { ClientModule } from '../client/client.module';
+import { StockMovementsRepository } from './stockmovements.repository';
+import { FileModule } from '../file/file.module';
+import { EmployeeModule } from '../employee/employee.module';
+import { EmployeeService } from '../employee/employee.service';
+import { StockTakeModule } from '../stocktake/stocktake.module';
+import { StockTakeService } from '../stocktake/stocktake.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: StockMovements.name, schema: StockMovementsSchema }]),
+    forwardRef(() => UsersModule),
+    forwardRef(() => CompanyModule),
+    forwardRef(() => RoleModule),
+    forwardRef(() => JobModule),
+    forwardRef(() => TeamModule),
+    forwardRef(() => ClientModule),
+    forwardRef(() => FileModule),
+    forwardRef(() => EmployeeModule),
+    forwardRef(() => StockTakeModule),
+  ],
+  controllers: [StockMovementsController],
+  providers: [StockMovementsService, StockMovementsRepository, JobService, EmployeeService, StockTakeService],
+  exports: [StockMovementsService, StockMovementsRepository, MongooseModule],
+})
+export class StockMovementsModule {}
