59227cdf49702b68606da473e13096dc4a9ffe8c
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-14 19:49:34 +0200

    refactor: Corrects schema types

--- src/api-gateway/src/chat/entities/chat-message.entity.ts
+++ src/api-gateway/src/chat/entities/chat-message.entity.ts
@@ -7,18 +7,19 @@ import { currentDate } from '../../utils/Utils';
 @Schema()
 export class ChatMessage {
   constructor(chatId: Types.ObjectId, userId: Types.ObjectId, textContent: string, attachments?: string[]) {
+    //this._id = new Types.ObjectId();
     this.chatId = chatId;
     this.userId = userId;
     this.textContent = textContent;
     if (attachments) this.attachments = attachments;
   }
-  @Prop({ type: SchemaTypes.ObjectId, required: true })
-  _id: Types.ObjectId = new Types.ObjectId();
+  // @Prop({ type: Types.ObjectId, required: true, default: new Types.ObjectId() })
+  // _id: Types.ObjectId = new Types.ObjectId();
 
-  @Prop({ type: Types.ObjectId, ref: Chat.name, required: true })
+  @Prop({ type: SchemaTypes.ObjectId, ref: Chat.name, required: true })
   chatId: Types.ObjectId;
 
-  @Prop({ type: Types.ObjectId, ref: User.name, required: true })
+  @Prop({ type: SchemaTypes.ObjectId, ref: User.name, required: true })
   userId: Types.ObjectId;
 
   @Prop({ type: String, required: true })
@@ -27,7 +28,7 @@ export class ChatMessage {
   @Prop({ type: Boolean, required: true, default: false })
   isRead: boolean = false;
 
-  @Prop({ type: [String], required: true, default: [] })
+  @Prop({ type: [String], required: false, default: [] })
   attachments?: string[] = [];
 
   @Prop({ default: Date.now })
