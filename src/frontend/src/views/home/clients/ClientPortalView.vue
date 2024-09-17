529b88a7916cbcf00d81a866f03266568c8791f0
JessicaBloem <jessicabloem8@gmail.com>
2024-09-17 07:38:28 +0200

    bugfix: some build issues fixed

--- src/frontend/src/views/home/clients/ClientPortalView.vue
+++ src/frontend/src/views/home/clients/ClientPortalView.vue
@@ -1,15 +1,26 @@
 <template>
-  <ClientPortal />
+  <ClientPortal :cid="cid" />
 </template>
+
 <script lang="ts">
-import { defineComponent } from 'vue'
-import ClientPortal from '@/components/home/clients/client_portal/ClientPortal.vue'
+import { defineComponent } from 'vue';
+import ClientPortal from '@/components/home/clients/client_portal/ClientPortal.vue';
+import { useRoute } from 'vue-router';  // Assuming cid comes from the route
 
 export default defineComponent({
   name: 'DashboardView',
 
   components: {
-    ClientPortal
-  }
-})
+    ClientPortal,
+  },
+
+  setup() {
+    const route = useRoute();
+    const cid = route.query.cid as string;  // Retrieve cid from query parameters
+
+    return {
+      cid,
+    };
+  },
+});
 </script>
