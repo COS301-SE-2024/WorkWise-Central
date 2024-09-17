217013eee9b467388d8528605224d701a2a815b7
JessicaBloem <jessicabloem8@gmail.com>
2024-09-17 12:41:09 +0200

    bugfix: build error fixed

--- src/frontend/src/components/home/invoices/ViewInvoices.vue
+++ src/frontend/src/components/home/invoices/ViewInvoices.vue
@@ -97,213 +97,213 @@ export default {
       this.generatePdf()
     },
     generatePdf() {
-      const props = {
-        outputType: OutputType.Blob, // Generate the PDF as a Blob to embed it
-        fileName: 'Invoice_2021',
-        orientationLandscape: false,
-        compress: true,
-        logo: {
-          src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png',
-          type: 'PNG',
-          width: 53.33,
-          height: 26.66,
-          margin: {
-            top: 0,
-            left: 0
-          }
-        },
-        stamp: {
-          inAllPages: true,
-          src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg',
-          type: 'JPG',
-          width: 20,
-          height: 20,
-          margin: {
-            top: 0,
-            left: 0
-          }
-        },
-        business: {
-          name: 'Business Name',
-          address: '1234 Business Street, City, Country',
-          phone: '(+355) 069 11 11 111',
-          email: 'email@example.com',
-          email_1: 'info@example.al',
-          website: 'www.example.al'
-        },
-        contact: {
-          label: 'Invoice issued for:',
-          name: 'Client Name',
-          address: '5678 Client Address, City, Country',
-          phone: '(+355) 069 22 22 222',
-          email: 'client@website.al',
-          otherInfo: 'www.website.al'
-        },
-        invoice: {
-          label: 'Invoice #: ',
-          num: 19,
-          invDate: 'Payment Date: 01/01/2021 18:12',
-          invGenDate: 'Invoice Date: 02/02/2021 10:17',
-          headerBorder: false,
-          tableBodyBorder: false,
-          header: [
-            { title: 'Invoice Number', style: { width: 10 } },
-            { title: 'Job Title', style: { width: 30 } },
-            { title: 'Client Name', style: { width: 80 } },
-            { title: 'Payment Date' },
-            { title: 'Paid' },
-            { title: 'Creation Date' },
-            { title: 'Total' }
-          ],
-          table: [
-            [1, 'Design Work', 'Initial design concept for website', 300, 2, 'hours', 600],
-            [2, 'Development', 'Frontend and backend development', 700, 5, 'hours', 3500],
-            [3, 'Testing', 'Testing and bug fixing', 200, 3, 'hours', 600]
-          ],
-          additionalRows: [
-            {
-              col1: 'Total:',
-              col2: '4700.00',
-              col3: 'USD',
-              style: {
-                fontSize: 14
-              }
-            },
-            {
-              col1: 'VAT:',
-              col2: '20',
-              col3: '%',
-              style: {
-                fontSize: 10
-              }
-            },
-            {
-              col1: 'SubTotal:',
-              col2: '3760.00',
-              col3: 'USD',
-              style: {
-                fontSize: 10
-              }
-            }
-          ],
-          invDescLabel: 'Invoice Note',
-          invDesc: 'Thank you for your business. Please make the payment by the due date.'
-        },
-        footer: {
-          text: 'The invoice is created on a computer and is valid without the signature and stamp.'
-        },
-        pageEnable: true,
-        pageLabel: 'Page '
-      }
+      // const props = {
+      //   outputType: OutputType.Blob, // Generate the PDF as a Blob to embed it
+      //   fileName: 'Invoice_2021',
+      //   orientationLandscape: false,
+      //   compress: true,
+      //   logo: {
+      //     src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png',
+      //     type: 'PNG',
+      //     width: 53.33,
+      //     height: 26.66,
+      //     margin: {
+      //       top: 0,
+      //       left: 0
+      //     }
+      //   },
+      //   stamp: {
+      //     inAllPages: true,
+      //     src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg',
+      //     type: 'JPG',
+      //     width: 20,
+      //     height: 20,
+      //     margin: {
+      //       top: 0,
+      //       left: 0
+      //     }
+      //   },
+      //   business: {
+      //     name: 'Business Name',
+      //     address: '1234 Business Street, City, Country',
+      //     phone: '(+355) 069 11 11 111',
+      //     email: 'email@example.com',
+      //     email_1: 'info@example.al',
+      //     website: 'www.example.al'
+      //   },
+      //   contact: {
+      //     label: 'Invoice issued for:',
+      //     name: 'Client Name',
+      //     address: '5678 Client Address, City, Country',
+      //     phone: '(+355) 069 22 22 222',
+      //     email: 'client@website.al',
+      //     otherInfo: 'www.website.al'
+      //   },
+      //   invoice: {
+      //     label: 'Invoice #: ',
+      //     num: 19,
+      //     invDate: 'Payment Date: 01/01/2021 18:12',
+      //     invGenDate: 'Invoice Date: 02/02/2021 10:17',
+      //     headerBorder: false,
+      //     tableBodyBorder: false,
+      //     header: [
+      //       { title: 'Invoice Number', style: { width: 10 } },
+      //       { title: 'Job Title', style: { width: 30 } },
+      //       { title: 'Client Name', style: { width: 80 } },
+      //       { title: 'Payment Date' },
+      //       { title: 'Paid' },
+      //       { title: 'Creation Date' },
+      //       { title: 'Total' }
+      //     ],
+      //     table: [
+      //       [1, 'Design Work', 'Initial design concept for website', 300, 2, 'hours', 600],
+      //       [2, 'Development', 'Frontend and backend development', 700, 5, 'hours', 3500],
+      //       [3, 'Testing', 'Testing and bug fixing', 200, 3, 'hours', 600]
+      //     ],
+      //     additionalRows: [
+      //       {
+      //         col1: 'Total:',
+      //         col2: '4700.00',
+      //         col3: 'USD',
+      //         style: {
+      //           fontSize: 14
+      //         }
+      //       },
+      //       {
+      //         col1: 'VAT:',
+      //         col2: '20',
+      //         col3: '%',
+      //         style: {
+      //           fontSize: 10
+      //         }
+      //       },
+      //       {
+      //         col1: 'SubTotal:',
+      //         col2: '3760.00',
+      //         col3: 'USD',
+      //         style: {
+      //           fontSize: 10
+      //         }
+      //       }
+      //     ],
+      //     invDescLabel: 'Invoice Note',
+      //     invDesc: 'Thank you for your business. Please make the payment by the due date.'
+      //   },
+      //   footer: {
+      //     text: 'The invoice is created on a computer and is valid without the signature and stamp.'
+      //   },
+      //   pageEnable: true,
+      //   pageLabel: 'Page '
+      // }
 
-      const doc = jsPDFInvoiceTemplate(props)
-      const pdfBlob = doc.blob
+      // const doc = jsPDFInvoiceTemplate(props)
+      // const pdfBlob = doc.blob
 
-      // Create an Object URL for the PDF to view in the iframe
-      this.pdfSrc = URL.createObjectURL(pdfBlob)
+      // // Create an Object URL for the PDF to view in the iframe
+      // this.pdfSrc = URL.createObjectURL(pdfBlob)
     },
     downloadPdf() {
-      const props = {
-        outputType: OutputType.Blob, // Generate the PDF as a Blob to embed it
-        fileName: 'Invoice_2021',
-        orientationLandscape: false,
-        compress: true,
-        logo: {
-          src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png',
-          type: 'PNG',
-          width: 53.33,
-          height: 26.66,
-          margin: {
-            top: 0,
-            left: 0
-          }
-        },
-        stamp: {
-          inAllPages: true,
-          src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg',
-          type: 'JPG',
-          width: 20,
-          height: 20,
-          margin: {
-            top: 0,
-            left: 0
-          }
-        },
-        business: {
-          name: 'Business Name',
-          address: '1234 Business Street, City, Country',
-          phone: '(+355) 069 11 11 111',
-          email: 'email@example.com',
-          email_1: 'info@example.al',
-          website: 'www.example.al'
-        },
-        contact: {
-          label: 'Invoice issued for:',
-          name: 'Client Name',
-          address: '5678 Client Address, City, Country',
-          phone: '(+355) 069 22 22 222',
-          email: 'client@website.al',
-          otherInfo: 'www.website.al'
-        },
-        invoice: {
-          label: 'Invoice #: ',
-          num: 19,
-          invDate: 'Payment Date: 01/01/2021 18:12',
-          invGenDate: 'Invoice Date: 02/02/2021 10:17',
-          headerBorder: false,
-          tableBodyBorder: false,
-          header: [
-            { title: 'Invoice Number', style: { width: 10 } },
-            { title: 'Job Title', style: { width: 30 } },
-            { title: 'Client Name', style: { width: 80 } },
-            { title: 'Payment Date' },
-            { title: 'Paid' },
-            { title: 'Creation Date' },
-            { title: 'Total' }
-          ],
-          table: [
-            [1, 'Design Work', 'Initial design concept for website', 300, 2, 'hours', 600],
-            [2, 'Development', 'Frontend and backend development', 700, 5, 'hours', 3500],
-            [3, 'Testing', 'Testing and bug fixing', 200, 3, 'hours', 600]
-          ],
-          additionalRows: [
-            {
-              col1: 'Total:',
-              col2: '4700.00',
-              col3: 'USD',
-              style: {
-                fontSize: 14
-              }
-            },
-            {
-              col1: 'VAT:',
-              col2: '20',
-              col3: '%',
-              style: {
-                fontSize: 10
-              }
-            },
-            {
-              col1: 'SubTotal:',
-              col2: '3760.00',
-              col3: 'USD',
-              style: {
-                fontSize: 10
-              }
-            }
-          ],
-          invDescLabel: 'Invoice Note',
-          invDesc: 'Thank you for your business. Please make the payment by the due date.'
-        },
-        footer: {
-          text: 'The invoice is created on a computer and is valid without the signature and stamp.'
-        },
-        pageEnable: true,
-        pageLabel: 'Page '
-      }
+    //   const props = {
+    //     outputType: OutputType.Blob, // Generate the PDF as a Blob to embed it
+    //     fileName: 'Invoice_2021',
+    //     orientationLandscape: false,
+    //     compress: true,
+    //     logo: {
+    //       src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png',
+    //       type: 'PNG',
+    //       width: 53.33,
+    //       height: 26.66,
+    //       margin: {
+    //         top: 0,
+    //         left: 0
+    //       }
+    //     },
+    //     stamp: {
+    //       inAllPages: true,
+    //       src: 'https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg',
+    //       type: 'JPG',
+    //       width: 20,
+    //       height: 20,
+    //       margin: {
+    //         top: 0,
+    //         left: 0
+    //       }
+    //     },
+    //     business: {
+    //       name: 'Business Name',
+    //       address: '1234 Business Street, City, Country',
+    //       phone: '(+355) 069 11 11 111',
+    //       email: 'email@example.com',
+    //       email_1: 'info@example.al',
+    //       website: 'www.example.al'
+    //     },
+    //     contact: {
+    //       label: 'Invoice issued for:',
+    //       name: 'Client Name',
+    //       address: '5678 Client Address, City, Country',
+    //       phone: '(+355) 069 22 22 222',
+    //       email: 'client@website.al',
+    //       otherInfo: 'www.website.al'
+    //     },
+    //     invoice: {
+    //       label: 'Invoice #: ',
+    //       num: 19,
+    //       invDate: 'Payment Date: 01/01/2021 18:12',
+    //       invGenDate: 'Invoice Date: 02/02/2021 10:17',
+    //       headerBorder: false,
+    //       tableBodyBorder: false,
+    //       header: [
+    //         { title: 'Invoice Number', style: { width: 10 } },
+    //         { title: 'Job Title', style: { width: 30 } },
+    //         { title: 'Client Name', style: { width: 80 } },
+    //         { title: 'Payment Date' },
+    //         { title: 'Paid' },
+    //         { title: 'Creation Date' },
+    //         { title: 'Total' }
+    //       ],
+    //       table: [
+    //         [1, 'Design Work', 'Initial design concept for website', 300, 2, 'hours', 600],
+    //         [2, 'Development', 'Frontend and backend development', 700, 5, 'hours', 3500],
+    //         [3, 'Testing', 'Testing and bug fixing', 200, 3, 'hours', 600]
+    //       ],
+    //       additionalRows: [
+    //         {
+    //           col1: 'Total:',
+    //           col2: '4700.00',
+    //           col3: 'USD',
+    //           style: {
+    //             fontSize: 14
+    //           }
+    //         },
+    //         {
+    //           col1: 'VAT:',
+    //           col2: '20',
+    //           col3: '%',
+    //           style: {
+    //             fontSize: 10
+    //           }
+    //         },
+    //         {
+    //           col1: 'SubTotal:',
+    //           col2: '3760.00',
+    //           col3: 'USD',
+    //           style: {
+    //             fontSize: 10
+    //           }
+    //         }
+    //       ],
+    //       invDescLabel: 'Invoice Note',
+    //       invDesc: 'Thank you for your business. Please make the payment by the due date.'
+    //     },
+    //     footer: {
+    //       text: 'The invoice is created on a computer and is valid without the signature and stamp.'
+    //     },
+    //     pageEnable: true,
+    //     pageLabel: 'Page '
+    //   }
 
-      const doc = jsPDFInvoiceTemplate(props)
-      doc.save('Invoice_2021.pdf') // Save the PDF as a downloadable file
+    //   const doc = jsPDFInvoiceTemplate(props)
+    //   doc.save('Invoice_2021.pdf') // Save the PDF as a downloadable file
     }
   }
 }
