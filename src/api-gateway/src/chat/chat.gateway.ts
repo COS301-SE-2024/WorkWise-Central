4f551025b0aa178782712f16771babd15f8edbc2
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-19 17:11:50 +0200

    fix: Removes all references to Async-api documentation to fix Api running

--- src/api-gateway/src/chat/chat.gateway.ts
+++ src/api-gateway/src/chat/chat.gateway.ts
@@ -17,7 +17,7 @@ import { CompanyService } from '../company/company.service';
 import { UsersService } from '../users/users.service';
 import { Types } from 'mongoose';
 import { DeleteChatDto } from './dto/delete-chat.dto';
-import { AsyncApiSub } from 'nestjs-asyncapi';
+//import { AsyncApiSub } from 'nestjs-asyncapi';
 
 //TODO: Check JWT expiration
 @WebSocketGateway({ cors: '*', namespace: 'chat-space' })
@@ -48,14 +48,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.logger.log(`Socket disconnected: ${socket.id}`);
   }
 
-  @AsyncApiSub({
+  /*  @AsyncApiSub({
     channel: 'init-chat',
     message: {
       payload: GetUserIdDto,
     },
     summary: 'Get all your ChatIds',
     description: `Used in order to retrieve all chats linked to a user after you connect to the websocket server.`,
-  })
+  })*/
   @SubscribeMessage('init-chat')
   async initChat(@ConnectedSocket() client: Socket, @MessageBody() payload: GetUserIdDto) {
     const decodedJwtAccessToken = this.jwtService.decode(payload.jwt);
@@ -69,14 +69,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     client.emit('init-chat', { chatIds: chatIds });
   }
 
-  @AsyncApiSub({
+/*  @AsyncApiSub({
     channel: 'new-message',
     message: {
       payload: AddMessageDto,
     },
     summary: 'Listen for new messages that are targeted to you as a user',
     description: `Used in order to receive new messages in real-time`,
-  })
+  })*/
   @SubscribeMessage('new-message')
   async handleNewMessage(@ConnectedSocket() client: Socket, @MessageBody(new ValidationPipe()) payload: AddMessageDto) {
     const userId = this.jwtService.decode(payload.jwt).sub;
@@ -91,14 +91,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.server.to(payload.chatId.toString()).emit('new-message', result);
   }
 
-  @AsyncApiSub({
+/*  @AsyncApiSub({
     channel: 'delete-message',
     message: {
       payload: DeleteMessageDto,
     },
     summary: 'Listen for removed messages in chats that are related to you as a user',
     description: `Used in order to receive  real-time updates from the chat gateway`,
-  })
+  })*/
   @SubscribeMessage('delete-message')
   async handleDeleteMessage(
     @ConnectedSocket() client: Socket,
@@ -116,14 +116,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.server.to(payload.chatId.toString()).emit('delete-message', result);
   }
 
-  @AsyncApiSub({
+/*  @AsyncApiSub({
     channel: 'delete-chat',
     message: {
       payload: DeleteChatDto,
     },
     summary: 'Listen for chats being deleted or "closed"',
     description: `Used in order to receive real-time feedback on chats that are still open`,
-  })
+  })*/
   @SubscribeMessage('delete-chat')
   async handleDeleteChat(@ConnectedSocket() client: Socket, @MessageBody(new ValidationPipe()) payload: DeleteChatDto) {
     const userId: Types.ObjectId = this.jwtService.decode(payload.jwt).sub;
@@ -138,14 +138,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.server.to(payload.chatId.toString()).emit('delete-chat', result);
   }
 
-  @AsyncApiSub({
+/*  @AsyncApiSub({
     channel: 'update-message',
     message: {
       payload: UpdateMessageDto,
     },
     summary: 'Listen for messages being edited by others in your chats',
     description: `Used in order to receive real-time feedback on chats that you are in`,
-  })
+  })*/
   @SubscribeMessage('update-message')
   async handleUpdateMessage(
     @ConnectedSocket() client: Socket,
--- src/api-gateway/src/main.ts
+++ src/api-gateway/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 import * as compression from 'compression';
 import { urlencoded, json } from 'express';
-import { createAsyncapiDocs } from './async-api';
+//import { createAsyncapiDocs } from './async-api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,7 +25,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await createAsyncapiDocs(app);
+//  await createAsyncapiDocs(app);
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
   app.use(compression());
