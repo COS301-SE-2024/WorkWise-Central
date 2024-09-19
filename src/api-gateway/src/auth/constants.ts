2d82da8302ef3a3f1c977379206cf2e784cce4f0
JessicaBloem <jessicabloem8@gmail.com>
2024-09-19 10:26:27 +0200

    refactor: added passphrase to company settings

--- src/api-gateway/src/auth/constants.ts
+++ src/api-gateway/src/auth/constants.ts
@@ -3,3 +3,7 @@ import * as process from 'node:process';
 export const jwtConstants = {
   secret: `${process.env.ANON_KEY}`,
 };
+
+export const paymentGatewayConstants = {
+  secret: `${process.env.ANON_KEY}`,
+};
