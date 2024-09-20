0e7c75c6a44ea45bab7fc546c52ee207f1702fbb
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-20 09:36:36 +0200

    chore: Imports confirmationService

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -11,20 +11,26 @@ import 'v-network-graph/lib/style.css'
 import './assets/styles.css'
 import 'vuetify/dist/vuetify.min.css'
 //import firebaseMessaging from '@/store/firebase'
-import { createPinia } from "pinia"
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import './index.css'
+import ConfirmationService from 'primevue/confirmationservice'
+
+//To refer to API in Dev and Prod
+export const API_URL: string = import.meta.env.VITE_SERVER_API
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
 //app.config.globalProperties.$messaging = firebaseMessaging
+
 app.use(KanbanPlugin)
 app.use(vuetify)
 app.use(router)
 app.use(ToastService)
 app.use(PrimeVue, { theme: { preset: Aura } })
 app.use(VNetworkGraph)
-app.use(pinia);
+app.use(pinia)
+app.use(ConfirmationService)
 // app.use(store)
 app.mount('#app')
