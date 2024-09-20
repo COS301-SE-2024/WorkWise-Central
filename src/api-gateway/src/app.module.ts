b8055305d1f83342391bfb316aced427c327e21d
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-20 09:30:57 +0200

    feat: Specifies uploads serving route

--- src/api-gateway/src/app.module.ts
+++ src/api-gateway/src/app.module.ts
@@ -20,6 +20,7 @@ import { StockTakeModule } from './stocktake/stocktake.module';
 import { ChatModule } from './chat/chat.module';
 import { TimeTrackerModule } from './time-tracker/time-tracker.module';
 import { StockMovementsModule } from './stockmovements/stockmovements.module';
+import { GlobalModuleModule } from './shared/global-module/global-module.module';
 
 const rootPath = join(__dirname, '..', '../../uploads');
 
@@ -43,10 +44,12 @@ const rootPath = join(__dirname, '..', '../../uploads');
     AdminModule,
     ServeStaticModule.forRoot({
       rootPath: rootPath,
+      serveRoot: '/uploads',
     }),
     ChatModule,
     TimeTrackerModule,
     StockMovementsModule,
+    GlobalModuleModule,
   ],
 })
 export class AppModule {}
