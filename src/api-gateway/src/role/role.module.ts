5f6377e8a9117e58f9f00ac79e0f6db6ae863926
JessicaBloem <jessicabloem8@gmail.com>
2024-09-22 12:58:13 +0200

    feature: inventory used added

--- src/api-gateway/src/role/role.module.ts
+++ src/api-gateway/src/role/role.module.ts
@@ -22,6 +22,7 @@ import { InventoryService } from '../inventory/inventory.service';
 import { InventoryModule } from '../inventory/inventory.module';
 import { StockTakeModule } from '../stocktake/stocktake.module';
 import { StockMovementsModule } from '../stockmovements/stockmovements.module';
+import { InventoryUsedModule } from '../inventory-used/inventory-used.module';
 
 @Module({
   imports: [
@@ -39,6 +40,7 @@ import { StockMovementsModule } from '../stockmovements/stockmovements.module';
     forwardRef(() => InventoryModule),
     forwardRef(() => StockTakeModule),
     forwardRef(() => StockMovementsModule),
+    forwardRef(() => InventoryUsedModule),
   ],
   controllers: [RoleController],
   providers: [
