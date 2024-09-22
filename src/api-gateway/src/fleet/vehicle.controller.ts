33ada314d5a2e31d321866e61cc9ee3e62e133e5
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-22 14:06:03 +0200

    feat: Documentation for Vehicle Service

--- src/api-gateway/src/fleet/vehicle.controller.ts
+++ src/api-gateway/src/fleet/vehicle.controller.ts
@@ -1,23 +1,28 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { VehicleInformationService } from './vehicle-information.service';
-import { VehicleInfo } from './models/vehicle-info.model';
-import { ApiTags } from '@nestjs/swagger';
+import { VehicleInfo, VehicleInfoSwagger } from './models/vehicle-info.model';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Vehicles')
 @Controller('vehicles')
 export class VehicleController {
   constructor(private readonly vehicleInformationService: VehicleInformationService) {}
 
+  @ApiResponse({ status: 200, description: 'Returns all vehicles', type: VehicleInfoSwagger, isArray: true })
   @Get('all')
   async getAllVehicles(): Promise<VehicleInfo[]> {
     return this.vehicleInformationService.getAllVehicles();
   }
 
+  @ApiResponse({ status: 201, description: 'Returns all vehicles', type: VehicleInfoSwagger })
+  @ApiBody({ type: VehicleInfoSwagger })
   @Post('add')
   async addVehicle(@Body() vehicle: VehicleInfo): Promise<VehicleInfo> {
     return this.vehicleInformationService.addVehicle(vehicle);
   }
 
+  @ApiResponse({ status: 201, description: 'Returns all vehicles', type: VehicleInfoSwagger })
+  @ApiBody({ type: VehicleInfoSwagger, isArray: true })
   @Post('add/bulk')
   async addMultipleVehicles(@Body() vehicles: VehicleInfo[]): Promise<VehicleInfo[]> {
     return this.vehicleInformationService.addMultipleVehicles(vehicles);
