e27df4fc475e2b06a3fdfbddd8e8c26857d85b0d
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-22 21:19:43 +0200

    feat: Integrates Map with Navbar

--- src/frontend/src/views/home/map/GoogleMapsView.vue
+++ src/frontend/src/views/home/map/GoogleMapsView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import SystemMap from '@/views/home/map/Map.vue'
-</script>
-
 <template>
-  <div style="border: 1px solid black">
-    <SystemMap />
-  </div>
+  <NavigationBar :component="Map" />
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NavigationBar from '@/components/home/NavigationBar.vue'
+import Map from '@/views/home/map/Map.vue'
+
+export default defineComponent({
+  name: 'MapView',
+  computed: {
+    Map() {
+      return Map
+    }
+  },
+  components: {
+    NavigationBar
+  }
+})
+</script>
