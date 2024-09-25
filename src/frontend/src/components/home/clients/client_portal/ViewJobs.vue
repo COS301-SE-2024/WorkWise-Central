ef377be06ab7f27bc7cafb68032d53f4ae7bcf6d
JessicaBloem <jessicabloem8@gmail.com>
2024-09-25 09:50:24 +0200

    refactor: frontend build error fixed, maybe

--- src/frontend/src/components/home/clients/client_portal/ViewJobs.vue
+++ src/frontend/src/components/home/clients/client_portal/ViewJobs.vue
@@ -44,7 +44,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="closeProgressModal">Close</v-btn>
+          <v-btn color="primary" @click="closeProgressModal">Close</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
