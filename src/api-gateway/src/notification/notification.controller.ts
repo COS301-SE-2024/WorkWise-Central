6a0353e6b9d85d9fb37742ac644cda08c146e605
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-25 08:58:51 +0200

    fix: Mark and unmark as read

--- src/api-gateway/src/notification/notification.controller.ts
+++ src/api-gateway/src/notification/notification.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Headers, HttpException, HttpStatus, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Headers, HttpException, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
 import { ApiBody, ApiInternalServerErrorResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { NotificationService } from './notification.service';
 import { Types } from 'mongoose';
@@ -94,8 +94,9 @@ export class NotificationController {
     } catch (e) {}
   }*/
 
+  @ApiOperation({ summary: 'Mark a notification as read' })
   @Patch('markAsRead/:nId')
-  async markAsRead(@Headers() headers: any, @Query('nId') nId: string) {
+  async markAsRead(@Headers() headers: any, @Param('nId') nId: string) {
     validateObjectId(nId);
     const notificationId = new Types.ObjectId(nId);
     try {
@@ -106,8 +107,9 @@ export class NotificationController {
     }
   }
 
+  @ApiOperation({ summary: 'Mark a notification as unread' })
   @Patch('markAsUnread/:nId')
-  async markAsUnread(@Headers() headers: any, @Query('nId') nId: string) {
+  async markAsUnread(@Headers() headers: any, @Param('nId') nId: string) {
     validateObjectId(nId);
     const notificationId = new Types.ObjectId(nId);
     try {
