e8f01fbed483b308a9d50ff5b0f13b2b6d90fa86
JessicaBloem <jessicabloem8@gmail.com>
2024-09-26 07:56:47 +0200

    refactor: styling on stock take updated

--- src/frontend/src/components/home/inventory/management/StockTakeManagement.vue
+++ src/frontend/src/components/home/inventory/management/StockTakeManagement.vue
@@ -1,182 +1,213 @@
 <template>
-  <v-container fluid fill-height>
-
-    <v-row class="justify-center align-center">
-      <v-col cols="12" class="text-center">
-        <h4>Stock Take</h4>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-container fluid fill-height>
-        <v-card class="bg-cardColor">
-          <v-card-title>
-            <v-row>
-              <v-col cols="12" lg="6">
-                <v-text-field
-                  v-model="searchQuery"
-                  label="Search"
-                  placeholder="Search for an item"
-                  outlined
-                  variant="outlined"
-                  color="primary"
-                  dense
-                  clearable
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" lg="6">
-                <v-text-field
-                  type="date"
-                  placeholder="Date of Stock Take"
-                  color="primary"
-                  variant="outlined"
-                  v-model="currentDate"
-                >
-                </v-text-field>
-              </v-col> </v-row
-          ></v-card-title>
-          <v-card-text>
-            <v-data-table
-              :headers="headers"
-              :items="filteredInventoryItems"
-              item-key="_id"
-              :row-props="getRowProps"
-              :header-props="{ class: 'bg-secondRowColor h6' }"
-              class="bg-cardColor"
-            >
-              <template v-slot:[`item.currentStockLevel`]="{ item }">
-                <v-text-field type="number" v-model="item.currentStockLevel"> </v-text-field>
-              </template>
-            </v-data-table>
-
-            <v-btn color="success" block @click="saveAllStockTake()">
-              <v-icon icon="fa: fa-solid fa-floppy-disk" color="success"></v-icon> Save
-            </v-btn></v-card-text
-          >
-        </v-card></v-container
-      >
-    </v-row>
-  </v-container>
-
-  <!-- Chart Dialog -->
-  <v-dialog v-model="chartDialog" max-width="600px">
-    <v-card class="bg-cardColor">
-      <v-card-title>Stock Levels for {{ selectedItem?.name }}</v-card-title>
-      <v-card-text>
-        <Chart type="bar" :data="chartData" />
-      </v-card-text>
-      <v-card-actions>
-        <v-container
-          ><v-row
-            ><v-col cols="12" lg="6">
-              <v-btn color="success" @click="generatePDF" block
-                ><v-icon icon="fa:fa-solid fa-file" color="success"></v-icon>Generate PDF</v-btn
-              ></v-col
-            ><v-col cols="12" lg="6">
-              <v-btn color="error" @click="chartDialog = false" block
-                ><v-icon icon="fa: fa-solid fa-cancel" color="error"></v-icon>Close</v-btn
-              ></v-col
-            ></v-row
-          ></v-container
+  <v-app :style="isDarkMode === true ? 'dark' : 'light'">
+    <v-container fluid fill-height>
+      <v-card height="auto" class="pa-11 ma-0 bg-cardColor" rounded="md" border="md">
+        <!-- Title and search area -->
+        <v-card-title
+          class="d-flex align-center pe-2 text-h5 font-weight-regular"
+          height="auto"
+          width="100%"
         >
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-  <div>
-    <!-- Button to open the stock take dialog -->
+          <v-row align="center" justify="space-between">
+            <v-col cols="12" lg="4" class="d-flex align-center">
+              <v-icon icon="fa: fa-solid fa-boxes"></v-icon>
+              <v-label class="ms-2 h2 font-family-Nunito text-headingTextColor">Stock Take</v-label>
+            </v-col>
 
-    <!-- Stock Take Dialog -->
-    <v-dialog v-model="showDialog" max-width="500px">
-      <v-card class="bg-cardColor">
-        <v-card-title>Record Stock of {{ selectedItem.name }}</v-card-title>
+            <!-- Search input -->
+            <v-col cols="12" lg="4" class="d-flex align-center">
+              <v-text-field
+                v-model="searchQuery"
+                density="compact"
+                label="Search"
+                prepend-inner-icon="mdi-magnify"
+                variant="outlined"
+                flat
+                color="primary"
+                width="100%"
+                hide-details="auto"
+                single-line
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-title>
+
+        <!-- Divider -->
+        <v-divider></v-divider>
+
+        <!-- Data Table and Save button -->
         <v-card-text>
-          <v-form @submit.prevent="submitStockTake">
-            <v-text-field
-              label="Name"
-              v-model="selectedItem.name"
-              :rules="nameRules"
-            ></v-text-field>
+          <v-data-table
+            :headers="headers"
+            :items="filteredInventoryItems"
+            item-key="_id"
+            :search="searchQuery"
+            height="auto"
+            class="bg-cardColor"
+            :row-props="getRowProps"
+            :header-props="{ class: 'bg-secondRowColor h6' }"
+          >
+            <template v-slot:[`item.currentStockLevel`]="{ item }">
+              <v-text-field type="number" v-model="item.currentStockLevel"></v-text-field>
+            </template>
+          </v-data-table>
 
-            <v-textarea
-              label="Description"
-              v-model="selectedItem.description"
-              :rules="descriptionRules"
-            ></v-textarea>
+          <v-row>
+            <!-- Date picker -->
+            <v-col cols="12" lg="6">
+              <h6 class="font-weight-bold">Date of Stock Take:</h6>
+              <!-- Heading for the date picker -->
+            </v-col>
+            <v-col cols="12" class="d-flex align-center">
+              <v-text-field
+                type="date"
+                placeholder="Date of Stock Take"
+                color="primary"
+                variant="outlined"
+                v-model="currentDate"
+                width="100%"
+              ></v-text-field>
+            </v-col>
 
-            <v-text-field
-              label="Cost Price"
-              v-model="selectedItem.costPrice"
-              type="number"
-              :rules="costPriceRules"
-            ></v-text-field>
+            <!-- Save button -->
+            <v-col cols="12" class="d-flex align-center justify-end">
+              <v-btn
+                class="text-none font-weight-regular hello"
+                color="success"
+                v-bind="activatorProps"
+                block
+                variant="elevated"
+                @click="saveAllStockTake"
+              >
+                <v-icon icon="fa: fa-solid fa-floppy-disk" color="white"></v-icon>
+                Save
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-container>
 
-            <v-text-field
-              label="Recorded Stock Level"
-              v-model="selectedItem.currentStockLevel"
-              type="number"
-              :rules="currentStockLevelRules"
-            ></v-text-field>
-
-            <v-text-field
-              label="Current Stock Level"
-              v-model="selectedItem.reorderLevel"
-              type="number"
-              :rules="reorderLevelRules"
-            ></v-text-field>
-
-            <v-file-input
-              v-model="selectedItem.images"
-              variant="solo"
-              accept="image/*"
-              width="100%"
-              placeholder="Inventory Item Image"
-              @change="handleImageUpload"
-              hint="Image size limit of  5MB"
-              persistent-hint
-              color="black"
-              rounded="md"
-              required
-              clearable
-              data-testid="company-logo-file-input"
-            ></v-file-input>
-          </v-form>
+    <!-- Chart Dialog -->
+    <v-dialog v-model="chartDialog" max-width="600px">
+      <v-card class="bg-cardColor">
+        <v-card-title>Stock Levels for {{ selectedItem?.name }}</v-card-title>
+        <v-card-text>
+          <Chart type="bar" :data="chartData" />
         </v-card-text>
         <v-card-actions>
           <v-container
-            ><v-row>
-              <v-col cols="12" lg="6" order-lg="2" order="1">
-                <v-btn color="success" @click="submitStockTake" block
-                  ><v-icon icon="fa: fa-solid fa-floppy-disk" color="success"></v-icon>Save</v-btn
+            ><v-row
+              ><v-col cols="12" lg="6">
+                <v-btn color="success" @click="generatePDF" block
+                  ><v-icon icon="fa:fa-solid fa-file" color="success"></v-icon>Generate PDF</v-btn
                 ></v-col
-              >
-              <v-col cols="12" lg="6" order-lg="1" order="2">
-                <v-btn color="error" @click="showDialog = false" block
-                  ><v-icon icon="fa: fa-solid fa-cancel" color="error"></v-icon>Cancel</v-btn
+              ><v-col cols="12" lg="6">
+                <v-btn color="error" @click="chartDialog = false" block
+                  ><v-icon icon="fa: fa-solid fa-cancel" color="error"></v-icon>Close</v-btn
                 ></v-col
-              >
-            </v-row></v-container
+              ></v-row
+            ></v-container
           >
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <template>
-      <!-- Confirmation Dialog with PDF Preview -->
-      <v-dialog v-model="confirmDialog" max-width="800px">
-        <v-card>
-          <v-card-title class="headline">Update Inventory</v-card-title>
+    <div>
+      <!-- Button to open the stock take dialog -->
+
+      <!-- Stock Take Dialog -->
+      <v-dialog v-model="showDialog" max-width="500px">
+        <v-card class="bg-cardColor">
+          <v-card-title>Record Stock of {{ selectedItem.name }}</v-card-title>
           <v-card-text>
-            <p>Do you want to update the inventory table based on this stock take?</p>
-            <iframe v-if="pdfUrl" :src="pdfUrl" width="100%" height="400px"></iframe>
+            <v-form @submit.prevent="submitStockTake">
+              <v-text-field
+                label="Name"
+                v-model="selectedItem.name"
+                :rules="nameRules"
+              ></v-text-field>
+
+              <v-textarea
+                label="Description"
+                v-model="selectedItem.description"
+                :rules="descriptionRules"
+              ></v-textarea>
+
+              <v-text-field
+                label="Cost Price"
+                v-model="selectedItem.costPrice"
+                type="number"
+                :rules="costPriceRules"
+              ></v-text-field>
+
+              <v-text-field
+                label="Recorded Stock Level"
+                v-model="selectedItem.currentStockLevel"
+                type="number"
+                :rules="currentStockLevelRules"
+              ></v-text-field>
+
+              <v-text-field
+                label="Current Stock Level"
+                v-model="selectedItem.reorderLevel"
+                type="number"
+                :rules="reorderLevelRules"
+              ></v-text-field>
+
+              <v-file-input
+                v-model="selectedItem.images"
+                variant="solo"
+                accept="image/*"
+                width="100%"
+                placeholder="Inventory Item Image"
+                @change="handleImageUpload"
+                hint="Image size limit of  5MB"
+                persistent-hint
+                color="black"
+                rounded="md"
+                required
+                clearable
+                data-testid="company-logo-file-input"
+              ></v-file-input>
+            </v-form>
           </v-card-text>
           <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="green darken-1" text @click="confirmUpdate(true)">Yes</v-btn>
-            <v-btn color="red darken-1" text @click="confirmUpdate(false)">No</v-btn>
+            <v-container
+              ><v-row>
+                <v-col cols="12" lg="6" order-lg="2" order="1">
+                  <v-btn color="success" @click="submitStockTake" block
+                    ><v-icon icon="fa: fa-solid fa-floppy-disk" color="success"></v-icon>Save</v-btn
+                  ></v-col
+                >
+                <v-col cols="12" lg="6" order-lg="1" order="2">
+                  <v-btn color="error" @click="showDialog = false" block
+                    ><v-icon icon="fa: fa-solid fa-cancel" color="error"></v-icon>Cancel</v-btn
+                  ></v-col
+                >
+              </v-row></v-container
+            >
           </v-card-actions>
         </v-card>
       </v-dialog>
-    </template>
-  </div>
+      <template>
+        <!-- Confirmation Dialog with PDF Preview -->
+        <v-dialog v-model="confirmDialog" max-width="800px">
+          <v-card>
+            <v-card-title class="headline">Update Inventory</v-card-title>
+            <v-card-text>
+              <p>Do you want to update the inventory table based on this stock take?</p>
+              <iframe v-if="pdfUrl" :src="pdfUrl" width="100%" height="400px"></iframe>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="green darken-1" text @click="confirmUpdate(true)">Yes</v-btn>
+              <v-btn color="red darken-1" text @click="confirmUpdate(false)">No</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </template>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -197,7 +228,7 @@ export default {
       headers: [
         { title: 'Name', value: 'name' },
 
-        { title: 'Current Stock Level', value:  'reorderLevel'},
+        { title: 'Current Stock Level', value: 'reorderLevel' },
         { title: 'Recorded Stock Level', value: 'currentStockLevel' },
         { title: '', value: 'actions', sortable: false }
       ],
@@ -225,7 +256,8 @@ export default {
       ],
       currentStockLevelRules: [
         (v) => !!v || 'Recorded Stock Level is required',
-        (v) => /^[+-]?(\d+(\.\d*)?|\.\d+)$/.test(v) || 'Recorded Stock Level must be a valid number',
+        (v) =>
+          /^[+-]?(\d+(\.\d*)?|\.\d+)$/.test(v) || 'Recorded Stock Level must be a valid number',
         (v) => !/^0\d/.test(v) || 'Recorded Stock Level cannot have leading zeros'
       ],
       reorderLevelRules: [
@@ -374,34 +406,34 @@ export default {
       this.pdfUrl = URL.createObjectURL(pdfBlob)
     },
     async confirmUpdate(update) {
-      this.updateInventory = update;
-      this.confirmDialog = false;
+      this.updateInventory = update
+      this.confirmDialog = false
       const config = {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${localStorage.getItem('access_token')}`
         }
-      };
+      }
       const data = {
-        "currentEmployeeId": localStorage.getItem('employeeId'),
-        "companyId": localStorage.getItem('currentCompany'),
-        "date": new Date(this.currentDate).toISOString(),
-        "items": [],
-        "updateInventory": this.updateInventory
-      };
+        currentEmployeeId: localStorage.getItem('employeeId'),
+        companyId: localStorage.getItem('currentCompany'),
+        date: new Date(this.currentDate).toISOString(),
+        items: [],
+        updateInventory: this.updateInventory
+      }
       this.filteredInventoryItems.forEach((item) => {
         data.items.push({
-          "inventoryId": item._id,
-          "recordedStockLevel": item.currentStockLevel
-        });
+          inventoryId: item._id,
+          recordedStockLevel: item.currentStockLevel
+        })
       })
       console.log('Data', data)
       try {
         const response = await axios.post(`${API_URL}stocktake/create`, data, config)
         console.log('Response:', response)
-        console.log("Hello world!");
+        console.log('Hello world!')
       } catch (error) {
-        console.log('Failure to update stock take', error);
+        console.log('Failure to update stock take', error)
       }
     },
 
@@ -526,7 +558,9 @@ export default {
       })
 
       autoTable(doc, {
-        head: [['Item Name', 'Cost Price', 'Recorded Stock Level', 'Current Stock Level', 'Difference']],
+        head: [
+          ['Item Name', 'Cost Price', 'Recorded Stock Level', 'Current Stock Level', 'Difference']
+        ],
         body: tableBody,
         startY: 40,
         theme: 'grid'
@@ -603,7 +637,7 @@ export default {
       } catch (error) {
         console.error(error)
       }
-    },
+    }
   },
   mounted() {
     this.getInventoryItems()
