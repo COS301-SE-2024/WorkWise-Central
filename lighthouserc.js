69dfbc04f0f2464a0b73b7bfcac72acf236e83a1
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-28 22:34:43 +0200

    chore: Adds new pages for Lighthouse tests

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,17 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: "./src/frontend/dist",
-      // url: ["http://localhost/5173"],
+      url: [
+        "http://localhost/",
+        "http://localhost/dashboard",
+        "http://localhost/chatHome",
+        "http://localhost/appointments",
+        "http://localhost/notifications",
+        "http://localhost/support",
+        "http://localhost/userSettings",
+        "http://localhost/manageCompanies",
+        "http://localhost/manageCompanies",
+      ],
       numberOfRuns: 3,
     },
     upload: {
