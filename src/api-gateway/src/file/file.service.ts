169e3338f63771023ee5c8b5f9a14c18b722754e
Hamza Mokiwa <u22575172@tuks.co.za>
2024-09-29 23:06:36 +0200

    feat: Adds AWS S3 storage

--- src/api-gateway/src/file/file.service.ts
+++ src/api-gateway/src/file/file.service.ts
@@ -1,14 +1,14 @@
 import { Global, Inject, Injectable } from '@nestjs/common';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import toStream = require('buffer-to-stream');
-import * as path from 'path';
-import * as fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
-import * as mime from 'mime-types';
-import { join } from 'path';
-import { SaveBase64Response } from './dto/SaveBase64Response.dto';
+//import * as path from 'path';
+//import * as fs from 'fs';
+//import { v4 as uuidv4 } from 'uuid';
+//import * as mime from 'mime-types';
+//import { join } from 'path';
+//import { SaveBase64Response } from './dto/SaveBase64Response.dto';
 import { ConfigService } from '@nestjs/config';
-
+import * as AWS from 'aws-sdk';
 //const uploadPath = join(__dirname, '..', '../../../uploads');
 
 @Global()
@@ -17,11 +17,56 @@ export class FileService {
   private readonly uploadPath: string;
   constructor(
     private configService: ConfigService,
-    @Inject('GLOBAL_CONFIG') private readonly globalConfig: { serverUrl: string; frontendUrl: string },
+    @Inject('GLOBAL_CONFIG')
+    private readonly globalConfig: { serverUrl: string; frontendUrl: string; aws_key: string; aws_secret: string },
   ) {
-    this.uploadPath = this.configService.get<string>('UPLOAD_PATH') || join(__dirname, '..', '../../../uploads');
+    //this.uploadPath = this.configService.get<string>('UPLOAD_PATH') || join(__dirname, '..', '../../../uploads');
+  }
+  ///
+  AWS_S3_BUCKET = 'work-wise-central';
+  s3 = new AWS.S3({
+    accessKeyId: this.globalConfig.aws_key,
+    secretAccessKey: this.globalConfig.aws_secret,
+  });
+
+  async uploadFileAWS(file) {
+    console.log(file);
+    const { originalname } = file;
+
+    return await this.s3_upload(file.buffer, this.AWS_S3_BUCKET, originalname, file.mimetype);
   }
 
+  async uploadFilesAWS(files: Express.Multer.File[]) {
+    const uploadPromises = files.map((file) => {
+      const { originalname } = file;
+      return this.s3_upload(file.buffer, this.AWS_S3_BUCKET, originalname, file.mimetype);
+    });
+
+    return await Promise.all(uploadPromises);
+  }
+
+  async s3_upload(file, bucket, name, mimetype) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+      ContentType: mimetype,
+      ContentDisposition: 'inline',
+      CreateBucketConfiguration: {
+        LocationConstraint: 'ap-south-1',
+      },
+    };
+
+    try {
+      const s3Response = await this.s3.upload(params).promise();
+      console.log(s3Response);
+      return s3Response.Location;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  ///
   async uploadBase64Image(base64Image: string): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
       const upload = v2.uploader.upload_stream((error, result) => {
@@ -47,6 +92,7 @@ export class FileService {
     });
   }
 
+  /*
   removeAfterLastDot(input: string): string | null {
     const lastDotIndex = input.lastIndexOf('.');
     if (lastDotIndex === -1) {
@@ -59,8 +105,9 @@ export class FileService {
     const lastDotIndex = input.lastIndexOf('.');
     return input.substring(lastDotIndex);
   }
+*/
 
-  async saveAttachments(files: Express.Multer.File[]): Promise<string[]> {
+  /*  async saveAttachments(files: Express.Multer.File[]): Promise<string[]> {
     if (!fs.existsSync(this.uploadPath)) {
       //console.log('FAILED TO FIND FILE');
       fs.mkdirSync(this.uploadPath, { recursive: true });
@@ -80,23 +127,23 @@ export class FileService {
     }
 
     return fileLinks;
-  }
+  }*/
 
   // getMimeTypeFromBase64(base64: string): string | null {
   //   const match = base64.match(/^data:(.*?);base64,/);
   //   return match ? match[1] : null;
   // }
 
-  getFileExtension(base64: string): string | false {
+  /*  getFileExtension(base64: string): string | false {
     const match = base64.match(/^data:(.*?);base64,/);
     if (!match) {
       return false;
     }
     const mimeType = match[1];
     return mime.extension(mimeType);
-  }
+  }*/
 
-  async saveBase64File(base64Image: string): Promise<SaveBase64Response> {
+  /*  async saveBase64File(base64Image: string): Promise<SaveBase64Response> {
     if (!fs.existsSync(this.uploadPath)) {
       fs.mkdirSync(this.uploadPath, { recursive: true });
     }
@@ -119,7 +166,7 @@ export class FileService {
     console.log('SAVING:', fileLink);
 
     return new SaveBase64Response(true, fileLink);
-  }
+  }*/
 
   /*
   async saveBase64Files(base64Files: { base64: string; originalname: string }[]): Promise<string[]> {
