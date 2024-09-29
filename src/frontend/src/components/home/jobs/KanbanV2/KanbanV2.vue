d26ba8b3533900354735bb69a3f65f469ace3dea
bob <siphelelebobdlamini@gmail.com>
2024-09-29 19:51:11 +0200

    fix: make request to backend for  jobs for a specific employee

--- src/frontend/src/components/home/jobs/KanbanV2/KanbanV2.vue
+++ src/frontend/src/components/home/jobs/KanbanV2/KanbanV2.vue
@@ -869,8 +869,7 @@ export default {
 
       try {
         const loaded_tags_response = await axios.get(
-          API_URL +
-            `job/all/company/detailed/${localStorage['currentCompany']}?currentEmployeeId=${localStorage.getItem('employeeId')}`,
+          API_URL + `job/all/employee/${localStorage.getItem('employeeId')}`,
           config
         )
         console.log(loaded_tags_response)
