9c08605b2f00ee224e0aa847c16fe4a0e8c015d0
Hamza Mokiwa <u22575172@tuks.co.za>
2024-10-01 10:13:43 +0200

    fix: Removed duplicate messages

--- src/frontend/src/components/home/notifications/management/NotificationsPage.vue
+++ src/frontend/src/components/home/notifications/management/NotificationsPage.vue
@@ -418,12 +418,12 @@ export default {
             console.error(error)
           }
 
-          this.$toast.add({
+          /*          this.$toast.add({
             severity: 'info',
             summary: 'Success',
             detail: 'Notification marked as read',
             life: 3000
-          })
+          })*/
         }
       }
     },
@@ -450,7 +450,7 @@ export default {
                 this.$toast.add({
                   severity: 'info',
                   summary: 'Success',
-                  detail: 'Notification marked as read',
+                  detail: 'Notification marked as unread',
                   life: 3000
                 })
               }
