95638c874285dbb7084ab3d7776d0559e744267e
bob <siphelelebobdlamini@gmail.com>
2024-10-20 09:10:14 +0200

    updates to request made to get the jobs to be the same as job table as requested for both Calendar.vue & KanbanV2.vue

--- src/frontend/src/components/home/jobs/KanbanV2/KanbanV2.vue
+++ src/frontend/src/components/home/jobs/KanbanV2/KanbanV2.vue
@@ -883,7 +883,7 @@ export default {
 
       try {
         const loaded_tags_response = await axios.get(
-          API_URL + `job/all/employee/${localStorage['employeeId']}`,
+          API_URL + `job/all/company/detailed/${localStorage['currentCompany']}`,
           config
         )
         console.log(loaded_tags_response)
